da70cf98486add786df3503229aceb5212ec5863
Vignard <Ronan>
2012-07-25 11:31:18 +0200

    Ajout des listes pour les 4 derniers types d'espèces.

--- scripts/liste_especes_zsc.php
+++ scripts/liste_especes_zsc.php
@@ -24,7 +24,10 @@ $departements = getDepartementsByIdRegional($id_regional);
 
 $amphibiens_reptiles = getAmpbhibiensReptilesByIdRegional($id_regional);
 $invertebres = getInvertebresByIdRegional($id_regional);
-
+$mammiferes = getMammiferesByIdRegional($id_regional);
+$plantes = getPlantesByIdRegional($id_regional);
+$poissons = getPoissonsByIdRegional($id_regional);
+$autres_especes = getAutresEspecesByIdRegional($id_regional);
 ?>
 <table class="cadre_plein">
     <tr>
@@ -77,13 +80,13 @@ $invertebres = getInvertebresByIdRegional($id_regional);
 <h3 class="spip">Amphibiens et reptiles&nbsp;:</h3>
 <table>
     <tr>
-        <th>Code  / Nom / Esp&egrave;ce de l'annexe 2</th>
+        <th>Code  | Nom | Esp&egrave;ce de l'annexe 2</th>
     </tr>
     <?php foreach ($amphibiens_reptiles as $amphibien_reptile): ?>
     <tr bgcolor="<?= switchcolor() ?>">
         <td>
-            <?= $amphibien_reptile["SPECNUM"] ?> / 
-            <em><?= $amphibien_reptile["SPECNAME"] ?></em> / 
+            <?= $amphibien_reptile["SPECNUM"] ?> | 
+            <em><?= $amphibien_reptile["SPECNAME"] ?></em> | 
             <?php if($amphibien_reptile["ANNEX_II"]=='Y'): ?>
             Oui
             <?php endif ?>
@@ -105,13 +108,13 @@ $invertebres = getInvertebresByIdRegional($id_regional);
 <h3 class="spip">Invert&eacute;br&eacute;s&nbsp;:</h3>
 <table>
     <tr>
-        <th>Code  / Nom / Esp&egrave;ce de l'annexe 2</th>
+        <th>Code  | Nom | Esp&egrave;ce de l'annexe 2</th>
     </tr>
     <?php foreach ($invertebres as $invertebre): ?>
     <tr bgcolor="<?= switchcolor() ?>">
         <td>
-            <?= $invertebre["SPECNUM"] ?> / 
-            <em><?= $invertebre["SPECNAME"] ?></em> / 
+            <?= $invertebre["SPECNUM"] ?> | 
+            <em><?= $invertebre["SPECNAME"] ?></em> | 
             <?php if($invertebre["ANNEX_II"]=='Y'): ?>
             Oui
             <?php endif ?>
@@ -128,135 +131,110 @@ $invertebres = getInvertebresByIdRegional($id_regional);
 /**
  *  3. Mammifères
  */
-		
-$query = "SELECT * FROM natura_mammal WHERE SITECODE = '$id_regional'";
-$result = mysql_query($query);
-$nombre = mysql_num_rows($result);
-
-if ($nombre != "0") {
-
-    echo "<h3 class=spip>Mammif&egrave;res&nbsp;:</h3><table><tr><td valign=top>";
-
-    $query = "SELECT * FROM natura_mammal WHERE SITECODE ='$id_regional'
-		AND ANNEX_II = 'Y' ORDER BY SPECNUM";
-    $result = mysql_query($query);
-
-    echo "<table><tr><th>Esp&egrave;ces de l'annexe 2&nbsp;:</th></tr>";
-
-    while ($val = mysql_fetch_array($result)) {
-        echo "<tr><td valign=top bgcolor='" . switchcolor() . "'>" . $val["SPECNUM"] . " " . utf8_encode($val["SPECNAME"]) . "</td></tr>";
-    }
-
-    echo "</table></td><td valign=top>";
-
-    $query = "SELECT * FROM natura_mammal WHERE SITECODE = '$id_regional'
-		AND ANNEX_II = '' ORDER BY SPECNUM";
-    $result = mysql_query($query);
-
-    echo "<table><tr><th>Autres esp&egrave;ces&nbsp;:</th></tr>";
-
-    while ($val = mysql_fetch_array($result)) {
-        echo "<tr><td valign=top bgcolor='" . switchcolor() . "'>" . $val["SPECNUM"] . " " . utf8_encode($val["SPECNAME"]) . "</td></tr>";
-    }
-
-    echo "</table></td></tr></table><br />";
-}
-
+?>		
+<?php if(count($mammiferes) > 0): ?>
+<h3 class="spip">Mammif&egrave;res&nbsp;:</h3>
+<table>
+    <tr>
+        <th>Code  | Nom | Esp&egrave;ce de l'annexe 2</th>
+    </tr>
+    <?php foreach ($mammiferes as $mammifere): ?>
+    <tr bgcolor="<?= switchcolor() ?>">
+        <td>
+            <?= $mammifere["SPECNUM"] ?> | 
+            <em><?= $mammifere["SPECNAME"] ?></em> | 
+            <?php if($mammifere["ANNEX_II"]=='Y'): ?>
+            Oui
+            <?php endif ?>
+            <?php if($mammifere["ANNEX_II"]==''): ?>
+            Non
+            <?php endif ?>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+</table>
+<br />
+<?php endif; ?>
+<?php
 /**
  *  4. Plantes
  */
-
-$query = "SELECT * FROM natura_plant WHERE SITECODE = '$id_regional'";
-$result = mysql_query($query);
-$nombre = mysql_num_rows($result);
-
-if ($nombre != "0") {
-
-    echo "<h3 class=spip>Plantes&nbsp;:</h3><table><tr><td valign=top>";
-
-    $query = "SELECT * FROM natura_plant WHERE SITECODE = '$id_regional'
-		AND ANNEX_II = 'Y' ORDER BY SPECNUM";
-    $result = mysql_query($query);
-
-    echo "<table><tr><th>Esp&egrave;ces de l'annexe 2&nbsp;:</th></tr>";
-
-    while ($val = mysql_fetch_array($result)) {
-        echo "<tr><td valign=top bgcolor='" . switchcolor() . "'>" . $val["SPECNUM"] . " " . utf8_encode($val["SPECNAME"]) . "</td></tr>";
-    }
-
-    echo "</table></td><td valign=top>";
-
-    $query = "SELECT * FROM natura_plant WHERE SITECODE = '$id_regional'
-		AND ANNEX_II = '' ORDER BY SPECNUM";
-    $result = mysql_query($query);
-
-    echo "<table><tr><th>Autres esp&egrave;ces&nbsp;:</th></tr>";
-
-    while ($val = mysql_fetch_array($result)) {
-        echo "<tr><td valign=top bgcolor='" . switchcolor() . "'>" . $val["SPECNUM"] . " " . utf8_encode($val["SPECNAME"]) . "</td></tr>";
-    }
-
-    echo "</table></td></tr></table><br />";
-}
-
+?>
+<?php if(count($plantes) > 0): ?>
+<h3 class="spip">Plantes&nbsp;:</h3>
+<table>
+    <tr>
+        <th>Code  | Nom | Esp&egrave;ce de l'annexe 2</th>
+    </tr>
+    <?php foreach ($plantes as $plante): ?>
+    <tr bgcolor="<?= switchcolor() ?>">
+        <td>
+            <?= $plante["SPECNUM"] ?> | 
+            <em><?= $plante["SPECNAME"] ?></em> | 
+            <?php if($plante["ANNEX_II"]=='Y'): ?>
+            Oui
+            <?php endif ?>
+            <?php if($plante["ANNEX_II"]==''): ?>
+            Non
+            <?php endif ?>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+</table>
+<br />
+<?php endif; ?>
+<?php
 /**
  *  5. Poissons
  */
-
-$query = "SELECT * FROM natura_fishes WHERE SITECODE = '$id_regional'";
-$result = mysql_query($query);
-$nombre = mysql_num_rows($result);
-
-if ($nombre != "0") {
-
-    echo "<h3 class=spip>Poissons&nbsp;:</h3><table><tr><td valign=top>";
-
-    $query = "SELECT * FROM natura_fishes WHERE SITECODE = '$id_regional'
-		AND ANNEX_II = 'Y' ORDER BY SPECNUM";
-    $result = mysql_query($query);
-
-    echo "<table><tr><th>Esp&egrave;ces de l'annexe 2&nbsp;:</th></tr>";
-
-    while ($val = mysql_fetch_array($result)) {
-        echo "<tr><td valign=top bgcolor='" . switchcolor() . "'>" . $val["SPECNUM"] . " " . utf8_encode($val["SPECNAME"]) . "</td></tr>";
-    }
-
-    echo "</table></td><td valign=top>";
-
-    $query = "SELECT * FROM natura_fishes WHERE SITECODE = '$id_regional'
-		AND ANNEX_II = '' ORDER BY SPECNUM";
-    $result = mysql_query($query);
-
-    echo "<table><tr><th>Autres esp&egrave;ces&nbsp;:</th></tr>";
-
-    while ($val = mysql_fetch_array($result)) {
-        echo "<tr><td valign=top bgcolor='" . switchcolor() . "'>" . $val["SPECNUM"] . " " . utf8_encode($val["SPECNAME"]) . "</td></tr>";
-    }
-
-    echo "</table></td></tr></table><br />";
-}
-
+?>
+<?php if(count($poissons) > 0): ?>
+<h3 class="spip">Poissons&nbsp;:</h3>
+<table>
+    <tr>
+        <th>Code  | Nom | Esp&egrave;ce de l'annexe 2</th>
+    </tr>
+    <?php foreach ($poissons as $poisson): ?>
+    <tr bgcolor="<?= switchcolor() ?>">
+        <td>
+            <?= $poisson["SPECNUM"] ?> | 
+            <em><?= $poisson["SPECNAME"] ?></em> | 
+            <?php if($poisson["ANNEX_II"]=='Y'): ?>
+            Oui
+            <?php endif ?>
+            <?php if($poisson["ANNEX_II"]==''): ?>
+            Non
+            <?php endif ?>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+</table>
+<br />
+<?php endif; ?>
+<?php
 /**
  *  6. Autres espèces
  */
-
-$query = "SELECT * FROM natura_spec WHERE SITECODE = '$id_regional'";
-$result = mysql_query($query);
-$nombre = mysql_num_rows($result);
-
-if ($nombre != "0") {
-
-    echo "<h3 class=spip>Autres&nbsp;:</h3>";
-
-    $query = "SELECT * FROM natura_spec WHERE SITECODE = '$id_regional' ORDER BY SPECNAME";
-    $result = mysql_query($query);
-
-    echo "<table>";
-
-    while ($val = mysql_fetch_array($result)) {
-        echo "<tr><td valign=top bgcolor='" . switchcolor() . "'>" . $val["SPECNUM"] . " " . utf8_encode($val["SPECNAME"]) . "</td></tr>";
-    }
-
-    echo "</table>";
-}
 ?>
+<?php if(count($autres_especes) > 0): ?>
+<h3 class="spip">Autres esp&egrave;ces&nbsp;:</h3>
+<table>
+    <tr>
+        <th>Nom | Esp&egrave;ce de l'annexe 2</th>
+    </tr>
+    <?php foreach ($autres_especes as $autre_espece): ?>
+    <tr bgcolor="<?= switchcolor() ?>">
+        <td>
+            <em><?= $autre_espece["SPECNAME"] ?></em> | 
+            <?php if($autre_espece["ANNEX_II"]=='Y'): ?>
+            Oui
+            <?php endif ?>
+            <?php if($autre_espece["ANNEX_II"]==''): ?>
+            Non
+            <?php endif ?>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+</table>
+<br />
+<?php endif; ?>
