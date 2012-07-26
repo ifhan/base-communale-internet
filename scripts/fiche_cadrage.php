08daa479383fb4d0a45a0d6c4d2ea9dcba4b8ed1
Vignard <Ronan>
2012-07-26 15:04:18 +0200

    Réécriture en OOP et formatage.

---
+++ scripts/fiche_cadrage.php
@@ -0,0 +1,58 @@
+<?php
+// Application-wide data and database connection
+require_once 'config/constants.inc.php';
+require_once 'config/database.inc.php';
+
+// Classes
+require_once 'classes/Commune.class.php';
+
+/**
+ *  Ce fichier sert à afficher les liens vers les ressources statistiques 
+ *  identifiées pour la commune
+ *  @var $id_commune Code géographique de la commune
+ */
+$id_commune = $_REQUEST["id_commune"];
+
+$commune = new Commune();
+$commune->getCommuneById($id_commune);
+?>
+<br />
+<div class="listerub">
+    <a name="risques">
+        <div class="titresousrub">Risques</div>
+    </a>
+    <div text="top">
+        <table>
+            <tr>
+                <td style="vertical-align:bottom;">
+                    <img src="IMG/png/gnome-globe.png" style="border:none" alt="Icone web" />
+                </td>
+                <td>
+                    <a href="<?=URL_PRIM_NET?><?=$commune->id_commune?>"  target="_blank">
+                       Consulter la fiche de la commune sur le site prim.net
+                    </a>.
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
+<br />
+<div class="listerub">
+    <a name="stats">
+        <div class="titresousrub">Statistiques</div>
+    </a>
+    <div text="top">
+        <table>
+            <tr>
+                <td style="vertical-align:bottom;">
+                    <img src="IMG/png/gnome-globe.png" style="border:none" alt="Icone web" />
+                </td>
+                <td>
+                    <a href='<?=URL_INSEE?><?=$commune->id_commune?>&Niveau=COM' target="_blank">
+                        Consulter les donn&eacute;es de cadrage de la commune sur le site de l'INSEE
+                    </a>.
+                </td>
+            </tr>
+        </table>
+    </div>
+</div>
