7720ce01e298c3408c2b2dcd10b7b2ed850d9a91
Vignard <Ronan>
2012-07-27 11:44:45 +0200

    Ajout de la fonction getSitesClassesInscrits()

--- scripts/classes/SiteClasseInscrit.class.php
+++ scripts/classes/SiteClasseInscrit.class.php
@@ -63,22 +63,41 @@ function getEntitesFromSiteByIdRegional($id_regional) {
  * @return array 
  */
 function getSiteClasseInscritPhotosByIdRegional($id_regional,$id_type) {
-        global $pdo;
-        $table = "R_SITE_CLASSE_INSCRIT_R52_photos";
-        $table_2 = "R_SITE_CLASSE_INSCRIT_R52";
-        $table_3 = "R_TYPE_ZONAGE_R52";
-        
-        $sql = "SELECT * 
-        FROM $table, $table_2, $table_3
-        WHERE $table.id_regional = '$id_regional'
-        AND $table.id_regional = $table_2.id_regional
-        AND $table_3.id_type = $id_type "; 
-        try {
-            $site_photos = $pdo->query($sql)->fetchAll();
-            return $site_photos;
+    global $pdo;
+    $table = "R_SITE_CLASSE_INSCRIT_R52_photos";
+    $table_2 = "R_SITE_CLASSE_INSCRIT_R52";
+    $table_3 = "R_TYPE_ZONAGE_R52";
+
+    $sql = "SELECT * 
+    FROM $table, $table_2, $table_3
+    WHERE $table.id_regional = '$id_regional'
+    AND $table.id_regional = $table_2.id_regional
+    AND $table_3.id_type = $id_type ";
+    try {
+        $site_photos = $pdo->query($sql)->fetchAll();
+        return $site_photos;
+    } catch (PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
+    }
+}
+
+/**
+ * Sélectionne l'ensemble des sites classés et inscrit de la région
+ * @global string $pdo
+ * @return array 
+ */
+function getSitesClassesInscrits() {
+    global $pdo;
+    $sql = "SELECT * 
+    FROM R_SITE_CLASSE_INSCRIT_R52 
+    GROUP BY id_regional 
+    ORDER BY id_regional";
+    try {
+            $sites_classes_inscrits = $pdo->query($sql)->fetchAll();
+            return $sites_classes_inscrits;
         } catch(PDOException $e) {
             echo 'ERROR: ' . $e->getMessage();
         }   
-    }
-
+}    
+    
 ?>
