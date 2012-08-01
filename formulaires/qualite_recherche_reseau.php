4e5bd2065f8ef9ffa691e85bd1f0fd6e250f57bd
Vignard <Ronan>
2012-08-01 11:22:26 +0200

    Formatage et modification des variables $reseau en $id_reseau et $departement en $id_dpt

---
+++ formulaires/qualite_recherche_reseau.php
@@ -0,0 +1,17 @@
+<?php
+function formulaires_qualite_recherche_reseau_traiter_dist(){
+    /**
+     * Permet d'utiliser le bouton "Précédent" pour revenir au formulaire
+     */
+    refuser_traiter_formulaire_ajax();
+    /**
+     * @var $id_reseau Identifiant du réseau
+     * @var $id_dpt Code géographique du département
+     */
+    $id_reseau = _request('id_reseau');
+    $id_dpt = _request('id_dpt');
+    return array(
+        'redirect' => 'spip.php?page=liste_stations_reseaux&reseau=' . $id_reseau . '&id_dpt=' . $id_dpt
+    );
+}
+?>
