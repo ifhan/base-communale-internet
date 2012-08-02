8c922ab8ffd9a892528b7f57830f3b62497291d5
Vignard <Ronan>
2012-08-02 14:18:43 +0200

    Formatage et réécriture en OOP

--- scripts/fiche_station_temperature.php
+++ scripts/fiche_station_temperature.php
@@ -3,71 +3,80 @@
 require_once 'config/constants.inc.php';
 require_once 'config/database.inc.php';
 
-// Utility functions  
-require_once 'classes/utilities.inc.php';
+// Classes
+require_once 'classes/StationTemperature.class.php';
+require_once 'classes/Zonage.class.php';
 
+/**
+ * @var $id_station Identifiant de la station
+ */
 $id_station = $_REQUEST["id_station"];
-$query = "SELECT map FROM local_types_zonages WHERE id_type = 28";
-$result = mysql_query($query);
-$val = mysql_fetch_assoc($result);
 
-echo "<div text=top><table><tr>";
-echo "<td style=vertical-align:bottom; ><img src=IMG/png/gnome-globe.png style=border:none alt=Icone web /></td>";
-echo "<td><a href=http://carmen.developpement-durable.gouv.fr/26/" . $val["map"] . ".map&object=stations_temperature_rcs;id_station;04" . $station;
-echo " target=_blank>Consulter la localisation de la station sur CARMEN";
-echo "</a>.</div></td></tr></table></div>";
+$station_temperature = new StationTemperature();
+$station_temperature->getStationTemperatureByIdStation($id_station);
 
-$query = "SELECT * FROM R_STATIONS_HYDROTEMPERATURE_R52 WHERE id_station = '$id_station' ";
-$result = mysql_query($query);
-$nombre = mysql_num_rows($result);
-
-echo "<h3 class=spip>Principales caract&eacute;ristiques de la station :</h3>";
-echo "<table class=spip>";
-echo "<thead><tr class=row_first><th>Identifiant</th><th>Cours d'eau</th><th>Nom de la station</th><th>Date de mise en service</th></tr></thead><tbody>";
-
-while ($val = mysql_fetch_assoc($result)) {
-
-    // Affichage du num�ro de la station, du nom du cours d'eau, du nom de la commune, de la localitisation pr�cise
-
-    echo "<tr bgcolor=" . switchcolor() . " valign=top><td>" . $val["id_station"] . "</td><td>";
-    echo utf8_encode(stripslashes($val["riviere"]));
-    echo "</td><td>" . utf8_encode($val["commune"]) . " (" . $val["id_commune"] . ") " . utf8_encode($val["localite"]);
-    echo "</td><td>" . date("d/m/Y", strtotime($val["en_service"])) . "</td></tr>";
-}
-
-echo "</tbody></table>";
-
-$dir = "data/fiches/temperature/";
-
-echo "<div class=listedoc><h3>T&eacute;l&eacute;charger :</h3><ul>";
-
-// Ouvre un dossier bien connu, et liste tous les fichiers
-
-if (@is_dir($dir)) {
-
-    if ($dh = @opendir($dir)) {
-
-        while (($file = @readdir($dh)) !== false) {
-
-            if ($file != "." && $file != "..") {
-
-                if (file_exists("data/fiches/temperature/$file/" . $id_station . ".pdf")) {
-
-                    echo "<li><a class=document href=data/fiches/temperature/$file/" . $id_station . ".pdf target=_blank>Temp&eacute;ratures en continu en " . $file . "</a>";
-                    echo "<span class=docformat><em>(PDF, ";
-                    echo @ConvertirTaille("data/fiches/temperature/$file/" . $id_station . ".pdf");
-                    echo ")</em></span></li>";
-                } else {
-                    echo "";
-                }
-            }
-        }
-
-        closedir($dh);
-    }
-}
-
-echo "</ul></div>";
-
-mysql_close();
+$zonage = new Zonage();
+$zonage->getTypeZonageByIdType("28");
 ?>
+<div text="top">
+    <table>
+        <tr>
+            <td style="vertical-align:bottom;">
+                <img src="IMG/png/gnome-globe.png" style="border:none" alt="Icone web" />
+            </td>
+            <td>
+                <a href="<?=URL_CARMEN?><?=$zonage->map?>.map&object=stations_temperature_rcs;id_station;04<?=$id_station?>" target="_blank">Consulter la localisation de la station sur CARMEN</a>.   
+            </td>
+        </tr>
+    </table>
+</div>
+<h3 class="spip">Principales caract&eacute;ristiques de la station :</h3>
+<table class="spip">
+    <thead>
+        <tr class="row_first">
+            <th>Identifiant</th>
+            <th>Cours d'eau</th>
+            <th>Nom de la station</th>
+            <th>Date de mise en service</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr valign="top">
+            <td><?=$station_temperature->id_station?></td>
+            <td><?=$station_temperature->riviere?></td>
+            <td><?=$station_temperature->commune?> (<?=$station_temperature->id_commune?>) <?=$station_temperature->localite?></td>
+            <td><?=$station_temperature->en_service?></td>
+        </tr>
+    </tbody>
+</table>
+<div class="listedoc">
+    <h3>T&eacute;l&eacute;charger :</h3>
+    <ul>
+<?php
+$dir = "data/fiches/temperature/";
+/**
+ *  Ouvre un dossier bien connu, et liste tous les fichiers
+ */
+if(@is_dir($dir)):
+    if($dh = @opendir($dir)):
+        while(($file = @readdir($dh)) !== false):
+            if($file != "." && $file != ".."):
+                if (file_exists("data/fiches/temperature/$file/" . $id_station . ".pdf")): ?>
+        <li>
+            <a class="document" 
+               href="data/fiches/temperature/<?=$file?>/<?=$id_station?>.pdf" target="_blank">Temp&eacute;ratures en continu en <?=$file?></a>
+            <span class=docformat>
+                <em>
+                    (PDF, <?=@ConvertirTaille("data/fiches/temperature/$file/" . $id_station . ".pdf")?>)
+                </em>
+            </span>
+        </li>
+            <?php endif;
+            endif;
+        endwhile;
+        closedir($dh);
+    endif;
+endif;
+?>
+    </ul>
+</div>
