0f7a00134945193040cbc9727d1c380c8251c31b
Vignard <Ronan>
2012-08-02 14:59:14 +0200

    Modification de la boucle if

--- scripts/inc/cours_eau.inc.php
+++ scripts/inc/cours_eau.inc.php
@@ -15,17 +15,16 @@ require_once(dirname(__FILE__)."/../classes/CoursEau.class.php");
 $id_riviere = $_REQUEST["id_riviere"];
 $id_station = $_REQUEST["id_station"];
 ?>
-<?php if($id_riviere!="0"): ?>
-    <?php
-    $riviere = new CoursEau();
-    $riviere->getRiviereByIdRiviere($id_riviere);
-    ?>
-    &nbsp;sur <?=$riviere->nom_riviere?>
-<?php endif; ?>
 <?php if($id_station!=""): ?>
     <?php
     $riviere = new CoursEau();
     $riviere->getRiviereByIdStation($id_station);
     ?>
     <?=$riviere->riviere?>
+<?php elseif($id_riviere!="0"): ?>
+    <?php
+    $riviere = new CoursEau();
+    $riviere->getRiviereByIdRiviere($id_riviere);
+    ?>
+    &nbsp;sur <?=$riviere->nom_riviere?>
 <?php endif; ?>
