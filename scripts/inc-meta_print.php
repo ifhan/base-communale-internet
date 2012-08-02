6487c38c45a007207c11ef7fc702bed4e6f2689d
Vignard <Ronan>
2012-08-02 15:12:39 +0200

    Formatage et suppression des balises </head> et <body> à réintégrer dans les gabarits PHP

--- scripts/inc-meta_print.php
+++ scripts/inc-meta_print.php
@@ -1,21 +1,8 @@
 <meta http-equiv="Content-Type" content="text/html; charset=#CHARSET">
-
 <!-- Feuilles de style -->
-
 <link rel="stylesheet" href="../css/spip_style.css" type="text/css" media="projection, screen, tv">
 <link rel="stylesheet" href="../css/style_intranet.css" type="text/css" media="print, projection, screen, tv">
 <link rel="stylesheet" href="../css/special_intranet.css" type="text/css" media="print, projection, screen, tv">
 <link rel="stylesheet" href="../css/print_intranet.css" type="text/css" media="print, projection, screen, tv">
-
 <!-- Favicon -->
-
 <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-
-</head>
-
-<body class="white">
-
-    <table id="cartouche" width="100%">
-        <tr>
-            <td><img src="../IMG/jpg/Bloc-Marques_MEEDDADT-150.jpg" alt="Logo MEEDDAT"></td>
-            <td valign="middle" width="90%">
