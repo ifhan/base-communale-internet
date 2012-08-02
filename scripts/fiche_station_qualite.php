9f3cc599bee364b98fd0693671b7fc6b254b02b0
Vignard <Ronan>
2012-08-02 14:47:56 +0200

    Formatage et réécriture en OOP

--- scripts/fiche_station_qualite.php
+++ scripts/fiche_station_qualite.php
@@ -6,95 +6,83 @@ require_once 'config/database.inc.php';
 // Utility functions  
 require_once 'classes/utilities.inc.php';
 
-// Récupération de l'identifiant de la station
-$station = $_REQUEST["station"];
+// Classes
+require_once 'classes/Zonage.class.php';
 
-$query = "SELECT map 
-    FROM R_TYPE_ZONAGE_R52 
-    WHERE id_type = 28";
-$result = mysql_query($query);
-$val = mysql_fetch_assoc($result);
+/**
+ * Ce fichier sert à afficher les liens vers l'ensemble des fichiers PDF
+ * concernant la station
+ * @var Identifiant de la station
+ */
+$id_station = $_REQUEST["id_station"];
+
+$zonage = new Zonage();
+$zonage->getTypeZonageByIdType("28");
 ?>
-
 <div text="top">
     <table>
         <tr>
             <td style="vertical-align:bottom;">
-                <img src="../IMG/png/gnome-globe.png" 
-                     style="border:none" alt="Icone web">
+                <img src="IMG/png/gnome-globe.png" style="border:none" alt="Icone web">
             </td>
             <td>
-                <a href="http://carmen.developpement-durable.gouv.fr/26/<?=$val["map"]?>.map
-                   &object=stations_qualite_rcs;
-                   id_station;04<?=$station?>" target="_blank">
-                    Consulter la localisation de la station sur CARMEN
-                </a>.    
+                <a href="<?=URL_CARMEN?><?=$zonage->map?>.map&object=stations_qualite_rcs;id_station;04<?=$id_station?>" target="_blank">Consulter la localisation de la station sur CARMEN</a>.    
             </td>
         </tr>
     </table>
 </div><br />
 <?php
 $dir = "data/fiches/qualite/";
-
-// Ouvre un dossier bien connu, et liste tous les fichiers
-if (@is_dir($dir)) {
-    if ($dh = @opendir($dir)) {
-        while (($file = @readdir($dh)) !== false) {
-            if ($file != "." && $file != "..") {
-                echo "<div class=listedoc>";
-                if ( (file_exists("data/fiches/qualite/$file/Physico-chimie/".$station.".pdf")) || (file_exists("data/fiches/qualite/$file/Pesticides/".$station.".pdf")) ) {
-					
-						echo "<h3>".$file." :</h3><ul>";					
-					
-					}
-					
-					if (file_exists("data/fiches/qualite/$file/Physico-chimie/".$station.".pdf")) {
-						
-						echo "<li><a class=document href='data/fiches/qualite/$file/Physico-chimie/".$station.".pdf' target=_blank>Physico-chimie</a>";
-						echo "<span class=docformat> <em>(PDF, ";
-						echo @ConvertirTaille("data/fiches/qualite/$file/Physico-chimie/".$station.".pdf");
-						echo ")</em></span></li>";
-
-					}
-
-					if (file_exists("data/fiches/qualite/$file/Pesticides/".$station.".pdf")) {
-
-						echo "<li><a class=document href='data/fiches/qualite/$file/Pesticides/".$station.".pdf' target=_blank>Pesticides</a>";
-						echo "<span class=docformat> <em>(PDF, ";
-						echo @ConvertirTaille("data/fiches/qualite/$file/Pesticides/".$station.".pdf");
-						echo ")</em></span></li>";
-
-					}
-
-					if (file_exists("data/fiches/qualite/$file/IBGN/".$station.".pdf")) {
-
-						echo "<li><a class=document href='data/fiches/qualite/$file/IBGN/".$station.".pdf' target=_blank>IBGN</a>";
-						echo "<span class=docformat> <em>(PDF, ";
-						echo @ConvertirTaille("data/fiches/qualite/$file/IBGN/".$station.".pdf");
-						echo ")</em></span></li>";
-
-					}
-
-					if (file_exists("data/fiches/qualite/$file/IBD/".$station.".pdf")) {
-
-						echo "<li><a class=document href='data/fiches/qualite/$file/IBD/".$station.".pdf' target=_blank>IBD</a>";
-						echo "<span class=docformat> <em>(PDF, ";
-						echo @ConvertirTaille("data/fiches/qualite/$file/IBD/".$station.".pdf");
-						echo ")</em></span></li>";
-
-					}
-
-					echo "</div>";
-
-				}
-
-			}
-
-			closedir($dh);
-
-		}
-
-	}
-			 
-	mysql_close();
-?>
+/**
+ *  Ouvre un dossier bien connu, et liste tous les fichiers
+ */
+if (@is_dir($dir)):
+    if ($dh = @opendir($dir)):
+        while (($file = @readdir($dh)) !== false):
+            if ($file != "." && $file != ".."): ?>
+<div class="listedoc">
+    <?php if((file_exists("data/fiches/qualite/$file/Physico-chimie/" . $id_station . ".pdf")) || (file_exists("data/fiches/qualite/$file/Pesticides/" . $id_station . ".pdf"))): ?>
+    <h3><?=$file?></h3>
+    <?php endif; ?>
+    <ul>
+        <?php if(file_exists("data/fiches/qualite/$file/Physico-chimie/" . $id_station . ".pdf")): ?>
+        <li><a class="document" 
+               href="data/fiches/qualite/$file/Physico-chimie/<?=$id_station?>.pdf" target="_blank">Physico-chimie</a>
+            <span class="docformat"> 
+                <em>(PDF, <?=@ConvertirTaille("data/fiches/qualite/$file/Physico-chimie/" . $id_station . ".pdf")?>)</em>
+            </span>
+        </li>
+        <?php endif; ?>
+        <?php if(file_exists("data/fiches/qualite/$file/Pesticides/" . $id_station . ".pdf")):  ?>
+        <li>
+            <a class="document" 
+               href="data/fiches/qualite/$file/Pesticides/<?=$id_station?>.pdf" target="_blank">Pesticides</a>
+            <span class="docformat">
+                <em>(PDF, <?=@ConvertirTaille("data/fiches/qualite/$file/Pesticides/" . $id_station . ".pdf")?>)</em>
+            </span>
+        </li>
+        <?php endif; ?>
+        <?php if(file_exists("data/fiches/qualite/$file/IBGN/" . $id_station . ".pdf")):  ?>
+        <li>
+            <a class="document" 
+               href="data/fiches/qualite/$file/IBGN/<?=$id_station?>.pdf" target="_blank">IBGN</a>
+            <span class="docformat">
+                <em>(PDF, <?=@ConvertirTaille("data/fiches/qualite/$file/IBGN/" . $id_station . ".pdf")?>)</em>
+            </span>
+        </li>
+        <?php endif; ?>
+        <?php if(file_exists("data/fiches/qualite/$file/IBD/" . $id_station . ".pdf")):  ?>
+        <li>
+            <a class="document" 
+               href="data/fiches/qualite/$file/IBD/<?=$id_station?>.pdf" target="_blank">IBD</a>
+            <span class="docformat">
+                <em>(PDF, <?=@ConvertirTaille("data/fiches/qualite/$file/IBD/" . $id_station . ".pdf")?>)</em>
+            </span>
+        </li>
+        <?php endif; ?>
+</div>
+    <?php endif;
+        endwhile;
+        closedir($dh);
+    endif;
+endif; ?>
