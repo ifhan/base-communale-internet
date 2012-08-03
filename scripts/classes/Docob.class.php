97683712ab39767db64c88d367f2a9b1c59a2a1c
Vignard <Ronan>
2012-08-03 11:53:37 +0200

    Formatage

--- scripts/classes/Docob.class.php
+++ scripts/classes/Docob.class.php
@@ -8,6 +8,12 @@
  * @version 1.0
  */
 class Docob {
+    
+    /**
+     * 
+     * @global string $pdo
+     * @return array 
+     */
     public function getDocob() {
         global $pdo;
         $sql = "(SELECT * FROM R_ZPS_R52_data) 
@@ -28,25 +34,30 @@ class Docob {
             echo 'ERROR: ' . $e->getMessage();
         }
     }
-   
+
 }
 
+/**
+ * 
+ * @global string $pdo
+ * @return array 
+ */
 function getDocob() {
-        global $pdo;        
-        $sql = "(SELECT * FROM R_ZPS_R52_data) 
+    global $pdo;
+    $sql = "(SELECT * FROM R_ZPS_R52_data) 
             UNION  (SELECT * FROM R_ZSC_R52_data) 
             UNION(SELECT * FROM R_SIC_R52_data) 
             ORDER BY id_regional";
-        try {
-            $docob = $pdo->query($sql)->fetchAll();
-            return $docob;
-            $this->id_regional = $docob["id_regional"];
-            $this->nom = $docob["nom"];
-            $this->id_article = $docob["id_article"];
-            $this->id_type = $docob["id_type"];
-        } catch(PDOException $e) {
-            echo 'ERROR: ' . $e->getMessage();
-        }   
+    try {
+        $docob = $pdo->query($sql)->fetchAll();
+        return $docob;
+        $this->id_regional = $docob["id_regional"];
+        $this->nom = $docob["nom"];
+        $this->id_article = $docob["id_article"];
+        $this->id_type = $docob["id_type"];
+    } catch (PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
     }
+}
 
 ?>
