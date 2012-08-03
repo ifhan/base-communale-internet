8dc7eca885411e1f49f5f17ef48936731b473099
Vignard <Ronan>
2012-08-03 12:03:14 +0200

    Formatage

--- scripts/classes/Scot.class.php
+++ scripts/classes/Scot.class.php
@@ -10,6 +10,7 @@
 class Scot {
     //put your code here
 }
+
 /**
  * Sélectionne le(s) commune(s) d'un SCOT par son identifiant
  * @global type $pdo
@@ -17,21 +18,22 @@ class Scot {
  * @return array 
  */
 function getCommunesScotByIdScot($id_scot) {
-        global $pdo;
-        $table = "BDC_COMMUNE_52";
-        $table_2 = "R_SCOT_COMMUNES_R52";
-        
-        $sql = "SELECT * 
+    global $pdo;
+    $table = "BDC_COMMUNE_52";
+    $table_2 = "R_SCOT_COMMUNES_R52";
+
+    $sql = "SELECT * 
         FROM $table, $table_2 
         WHERE $table_2.id_scot = $id_scot
         AND $table.id_commune = $table_2.id_commune
         GROUP BY $table.id_commune 
         ORDER BY $table.id_commune";
-        try {
-            $communes = $pdo->query($sql)->fetchAll();
-            return $communes;
-        } catch(PDOException $e) {
-            echo 'ERROR: ' . $e->getMessage();
-        }    
+    try {
+        $communes = $pdo->query($sql)->fetchAll();
+        return $communes;
+    } catch (PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
     }
+}
+
 ?>
