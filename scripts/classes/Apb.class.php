c10b19384cc4b175349c02d363fd43c0ce08785d
Vignard <Ronan>
2012-08-03 11:50:04 +0200

    Formatage

--- scripts/classes/Apb.class.php
+++ scripts/classes/Apb.class.php
@@ -8,7 +8,7 @@
  * @version 1.0
  */
 class Apb {
-    
+
     /**
      * Sélectionne un APB par son identifiant régional
      * @global string $pdo
@@ -22,18 +22,18 @@ class Apb {
         AND R_APB_R52.id_regional = R_APB_R52_data.id_regional ";
         try {
             $row = $pdo->query($sql)->fetch();
-            $this->id_regional  = $row['id_regional'];
+            $this->id_regional = $row['id_regional'];
             $this->id_national = $row['id_national'];
             $this->nom = $row['nom'];
-            $this->code_arrete  = $row['code_arrete'];
-            $this->date  = date("d/m/Y", strtotime($row['date']));
-            $this->commentaire_arrete  = $row['commentaire_arrete'];
-            $this->surf_sig_l93  = $row['surf_sig_l93'];
-            $this->parcelles  = nl2br($row['parcelles']);
-            $this->statut_foncier  = nl2br($row['statut_foncier']);
-            $this->interet_bio  = nl2br($row['interet_bio']);
-            $this->effets_protection  = nl2br($row['effets_protection']);
-        } catch(PDOException $e) {
+            $this->code_arrete = $row['code_arrete'];
+            $this->date = date("d/m/Y", strtotime($row['date']));
+            $this->commentaire_arrete = $row['commentaire_arrete'];
+            $this->surf_sig_l93 = $row['surf_sig_l93'];
+            $this->parcelles = nl2br($row['parcelles']);
+            $this->statut_foncier = nl2br($row['statut_foncier']);
+            $this->interet_bio = nl2br($row['interet_bio']);
+            $this->effets_protection = nl2br($row['effets_protection']);
+        } catch (PDOException $e) {
             echo 'ERROR: ' . $e->getMessage();
         }
     }
