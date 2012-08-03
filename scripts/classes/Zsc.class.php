ce6d58247a1ebb4bf1c456338e8da69067d31e3c
Vignard <Ronan>
2012-08-03 12:11:09 +0200

    Formatage

--- scripts/classes/Zsc.class.php
+++ scripts/classes/Zsc.class.php
@@ -9,9 +9,7 @@
  * @version 1.0
  */
 class Zsc {
-    public $id_regional;
-    public $nom;
-    
+
     /**
      * Sélectionne les données annexes d'une ZSC
      * @global type $pdo
@@ -19,21 +17,21 @@ class Zsc {
      */
     public function getZscDataByIdRegional($id_regional) {
         global $pdo;
-        $sql ="SELECT * 
+        $sql = "SELECT * 
         FROM R_ZSC_R52_data 
         WHERE id_regional = '$id_regional' ";
         try {
-            $zsc = $pdo->query($sql)->fetch();          
+            $zsc = $pdo->query($sql)->fetch();
             $this->id_regional = $zsc["id_regional"];
             $this->nom = $zsc["nom"];
             $this->dreal = $zsc["dreal"];
             $this->url_fiche = $zsc["url_fiche"];
             $this->url_docob = $zsc["url_docob"];
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             echo 'ERROR: ' . $e->getMessage();
-        }    
-        
+        }
     }
+
 }
 
 ?>
