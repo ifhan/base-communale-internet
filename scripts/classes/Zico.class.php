9ef1f5f979ceee1a2418b0e129d4eb0629156cc8
Vignard <Ronan>
2012-08-03 12:08:09 +0200

    Formatage

--- scripts/classes/Zico.class.php
+++ scripts/classes/Zico.class.php
@@ -9,6 +9,7 @@
  * @version 1.0
  */
 class Zico {
+
     /**
      * Sélectionne une ZICO par son identifiant régional
      * @global string $pdo
@@ -24,22 +25,23 @@ class Zico {
             $row = $pdo->query($sql)->fetch();
             $this->id_regional = $row['id_regional'];
             $this->nom = $row['nom'];
-            $this->annee_description  = date("Y", strtotime($row['annee_description']));
-            $this->date_maj  = date("d/m/Y", strtotime($row['date_maj']));
-            $this->altitude_min  = $row['altitude_min'];
-            $this->altitude_max  = $row['altitude_max'];
+            $this->annee_description = date("Y", strtotime($row['annee_description']));
+            $this->date_maj = date("d/m/Y", strtotime($row['date_maj']));
+            $this->altitude_min = $row['altitude_min'];
+            $this->altitude_max = $row['altitude_max'];
             $this->surf_sig_l93 = $row['surf_sig_l93'];
-            $this->informations_complementaires  = nl2br($row['informations_complementaires']);
-            $this->interet_milieu  = nl2br($row['interet_milieu']);
-            $this->protections_reglementaires  = nl2br($row['protections_reglementaires']);
-            $this->mesures_foncieres  = nl2br($row['mesures_foncieres']);
-            $this->mesures_gestion  = nl2br($row['mesures_gestion']);
-            $this->menaces  = nl2br($row['menaces']);
-            $this->sources  = nl2br($row['sources']);
-        } catch(PDOException $e) {
+            $this->informations_complementaires = nl2br($row['informations_complementaires']);
+            $this->interet_milieu = nl2br($row['interet_milieu']);
+            $this->protections_reglementaires = nl2br($row['protections_reglementaires']);
+            $this->mesures_foncieres = nl2br($row['mesures_foncieres']);
+            $this->mesures_gestion = nl2br($row['mesures_gestion']);
+            $this->menaces = nl2br($row['menaces']);
+            $this->sources = nl2br($row['sources']);
+        } catch (PDOException $e) {
             echo 'ERROR: ' . $e->getMessage();
-        }   
+        }
     }
+
 }
 
 ?>
