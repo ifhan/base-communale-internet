2be22eb60e1fd6b093d92dc892d306b569fbf02f
Vignard <Ronan>
2012-08-03 12:12:56 +0200

    Formatage

--- scripts/classes/utilities.inc.php
+++ scripts/classes/utilities.inc.php
@@ -1,54 +1,48 @@
 <?php
 
 /**
- * Retourne la chaine passee en parametre dans un tableau de mots en conservant l'ordre de la chaine
- * 
+ * Retourne la chaîne passée en paramètre dans un tableau de mots en 
+ * conservant l'ordre de la chaîne
  * @param type $string
  * @return type 
  */
-function split_words($string)
-{ 
-     $retour = array();
-     $delimiteurs = " .!?,:(){}[]%\/";
-     $tok = strtok($string, " ");
-     while (strlen(join(" ", $retour)) != strlen($string))
-     { 
-          array_push($retour, $tok);
-          $tok = strtok ($delimiteurs); 
-     }
-     return array_non_empty($retour);
+function split_words($string) {
+    $retour = array();
+    $delimiteurs = " .!?,:(){}[]%\/";
+    $tok = strtok($string, " ");
+    while (strlen(join(" ", $retour)) != strlen($string)) {
+        array_push($retour, $tok);
+        $tok = strtok($delimiteurs);
+    }
+    return array_non_empty($retour);
 }
 
 /**
- * Supprime les entrees vides du tableau
- * 
+ * Supprime les entrées vides du tableau
  * @param type $array
  * @return array 
  */
-function array_non_empty($array)
-{
-     $retour = array();
-     foreach ($array as $a)
-     {
-          if ((!empty($a)) && (strlen($a) >= 3)) { array_push($retour, trim($a)); }
-     }
-     return $retour;
+function array_non_empty($array) {
+    $retour = array();
+    foreach ($array as $a) {
+        if ((!empty($a)) && (strlen($a) >= 3)) {
+            array_push($retour, trim($a));
+        }
+    }
+    return $retour;
 }
 
 /**
- * Gestion centralisee des messages d'erreurs
- * 
+ * Gestion centralisée des messages d'erreurs
  * @param type $niveau
  * @param type $message 
  */
-function gestionnaire_erreurs($niveau, $message)
-{
-    
-/*
- * Affiche simplement un message
- */
-    if($niveau == 2)
-    {
+function gestionnaire_erreurs($niveau, $message) {
+
+    /*
+     * Affiche simplement un message
+     */
+    if ($niveau == 2) {
         echo "<em>";
         echo "Le fichier n'existe pas ou le nom du fichier ne correspond pas.";
         echo "</em>";
@@ -57,65 +51,54 @@ function gestionnaire_erreurs($niveau, $message)
 
 /**
  * Calcule la taille du fichier
- * 
  * @param type $filename 
  */
-function ConvertirTaille($filename)
-{
-    $data=filesize($filename);
+function ConvertirTaille($filename) {
+    $data = filesize($filename);
     if ($data < 1024)
-		echo "$data";
-	elseif ($data >= 1024 && $data < (1<<20)) 
-		echo round(($data / 1024), 0)." Ko"; 
-	elseif($data >= (1<<20) && $data < (1<<30)) 
-		echo round(($data / (1<<20)), 2)." Mo";
-	else
-		echo round(($data / (1<<30)), 2)." Go";
+        echo "$data";
+    elseif ($data >= 1024 && $data < (1 << 20))
+        echo round(($data / 1024), 0) . " Ko";
+    elseif ($data >= (1 << 20) && $data < (1 << 30))
+        echo round(($data / (1 << 20)), 2) . " Mo";
+    else
+        echo round(($data / (1 << 30)), 2) . " Go";
 }
 
 /**
  * Alterne les couleurs des lignes dans un tableau
- * 
  * @staticvar type $col
  * @return string 
  */
-function switchcolor()
-{ 
+function switchcolor() {
     static $col;
     $couleur1 = "#ffffff";
     $couleur2 = "#eeeeee";
 
-    if($col == $couleur1){
+    if ($col == $couleur1) {
         $col = $couleur2;
-    }
-    
-    else {
+    } else {
         $col = $couleur1;
     }
-    
-    return $col; 
- }
+    return $col;
+}
 
 /**
  * Formate une date
- * 
  * @param type $date
  * @return type 
  */
-function dd($date)
-{
-    return date("d/m/Y",$date);
+function dd($date) {
+    return date("d/m/Y", $date);
 }
 
 /**
  *  Affiche l'extension d'un ficher
- * 
  * @param type $file
  * @return type 
  */
-function extension($file)
-{
-    return substr($file, strrpos($file, ".")+1);
+function extension($file) {
+    return substr($file, strrpos($file, ".") + 1);
 }
 
 ?>
