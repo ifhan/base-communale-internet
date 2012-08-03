d211d5d1d942ae0aac5533287fb31efe2188c118
Vignard Ronan <>
2012-08-03 16:41:10 +0200

    Commit initial

---
+++ scripts/fiche_pnr.php
@@ -0,0 +1,60 @@
+<?php
+// Application-wide data and database connection
+require_once 'config/constants.inc.php';
+require_once 'config/database.inc.php';
+
+// Classes
+require_once 'classes/Departement.class.php';
+require_once 'classes/Pnr.class.php';
+
+/**
+ * Ce fichier sert à afficher la fiche descriptive d'un PNR
+ * @var $id_regional Identifiant régional du zonage
+ */
+$id_regional = $_REQUEST["id_regional"];
+
+$pnr = new Pnr();
+$pnr->getPnrByIdRegional($id_regional);
+
+$departements = getDepartementsByIdRegional($id_regional);
+?>
+<table class="cadre_plein">
+    <tr>
+        <td>Nom&nbsp;:</td>
+        <td><strong><?=$pnr->nom?></strong></td>
+    </tr>
+    <tr>
+        <td>Identifiant r&eacute;gional&nbsp;:</td>
+        <td><strong><?=$pnr->id_regional?></strong></td>
+    </tr>
+    <tr>
+        <td>Surface calcul&eacute;e dans le SIG&nbsp;:</td>
+        <td><strong><?=$pnr->surf_sig_l93?> ha</strong></td>
+    </tr>
+    <tr>
+        <td valign="top">D&eacute;partement(s)&nbsp;:</td>
+        <td>
+            <?php foreach($departements as $departement): ?>
+            <strong>
+            <?=$departement["nom_departement"]?> (<?=$departement["id_departement"]?>)
+            </strong>
+            <br />
+            <?php endforeach;?>
+        </td>
+    </tr>
+</table>
+<h3 class="spip">Commune(s) concern&eacute;e(s) en Pays de la Loire&nbsp;:</h3>
+<?php require_once 'inc/commune.inc.php'; ?>
+<h3 class="spip">Lien&nbsp;:</h3>
+<div text="top">
+    <table>
+        <tr>
+            <td style="vertical-align:bottom;">
+                <img src="IMG/png/gnome-globe.png" style="border:none" alt="Icone web" />
+            </td>
+            <td>
+                <a href="<?=$pnr->url_site?>" target="_blank">Consulter le site du Parc Naturel R&eacute;gional</a>.   
+            </td>
+        </tr>
+    </table>
+</div>
