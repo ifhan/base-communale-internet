ade38f2708c9c71ff1b6e61412f42ae5bdca20c6
Vignard <Ronan>
2012-08-03 12:01:33 +0200

    Formatage et doc

--- scripts/classes/Rnr.class.php
+++ scripts/classes/Rnr.class.php
@@ -8,24 +8,27 @@
  * @version 1.0
  */
 class Rnr {
-    public $id_regional;
-    public $nom;
-    public $url_site;
-    
-    public function getRnrByIdRegional($id_regional){
+
+    /**
+     * Sélectionne une RNR par son identifiant régional
+     * @global string $pdo
+     * @param int $id_regional 
+     */
+    public function getRnrByIdRegional($id_regional) {
         global $pdo;
         $sql = "SELECT * 
         FROM R_RNR_R52 
         WHERE id_regional = '$id_regional' ";
         try {
-            $rnr = $pdo->query($sql)->fetch();          
+            $rnr = $pdo->query($sql)->fetch();
             $this->id_regional = $rnr["id_regional"];
             $this->nom = $rnr["nom"];
             $this->url_site = $rnr["url_site"];
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             echo 'ERROR: ' . $e->getMessage();
-        }    
+        }
     }
+
 }
 
 ?>
