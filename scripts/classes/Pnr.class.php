d300ba0e018a083833f65b88ffd7743e9bedc611
Vignard <Ronan>
2012-08-03 11:59:10 +0200

    Formatage et doc

--- scripts/classes/Pnr.class.php
+++ scripts/classes/Pnr.class.php
@@ -8,24 +8,27 @@
  * @version 1.0
  */
 class Pnr {
-    public $id_regional;
-    public $nom;
-    public $url_site;
-    
-    public function getPnrByIdRegional($id_regional){
+
+    /**
+     * Sélectionne un PNR par son identifiant régional
+     * @global string $pdo
+     * @param string $id_regional 
+     */
+    public function getPnrByIdRegional($id_regional) {
         global $pdo;
         $sql = "SELECT * 
         FROM R_PNR_R52 
         WHERE id_regional = '$id_regional' ";
         try {
-            $pnr = $pdo->query($sql)->fetch();          
+            $pnr = $pdo->query($sql)->fetch();
             $this->id_regional = $pnr["id_regional"];
             $this->nom = $pnr["nom"];
             $this->url_site = $pnr["url_site"];
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             echo 'ERROR: ' . $e->getMessage();
-        }    
+        }
     }
+
 }
 
 ?>
