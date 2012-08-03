74eec2de41cd9b7359c68a7da7635c4c7379a83c
Vignard <Ronan>
2012-08-03 11:48:51 +0200

    Formatage

--- scripts/oiseaux_proteges_france_2009.php
+++ scripts/oiseaux_proteges_france_2009.php
@@ -1,7 +1,7 @@
 <?php
 // Application-wide data and database connection
 require_once 'config/constants.inc.php';
-require_once 'config/database.inc.php';	
+require_once 'config/database.inc.php';
 
 // Utility functions  
 require_once 'classes/utilities.inc.php';
@@ -17,8 +17,8 @@ $id_article = $_REQUEST["id_article"];
 
 $oiseaux_proteges = getOiseauxProtegesByIdArticle($id_article);
 ?>
-<?php if($id_article!=="0"): ?>
-<p>Esp&egrave;ces list&eacute;es &agrave; l'article <?=$id_article?> de l'arr&ecirc;t&eacute; du 29 octobre 2009 :</p><br />
+<?php if ($id_article !== "0"): ?>
+    <p>Esp&egrave;ces list&eacute;es &agrave; l'article <?= $id_article ?> de l'arr&ecirc;t&eacute; du 29 octobre 2009 :</p><br />
 <?php endif; ?>
 <table class="display" id="example">
     <thead>
@@ -31,19 +31,19 @@ $oiseaux_proteges = getOiseauxProtegesByIdArticle($id_article);
         </tr>
     </thead>
     <tbody>
-        <?php foreach($oiseaux_proteges as $oiseau_protege): ?>
-        <tr>
-            <td><?=$oiseau_protege["id"]?></td>
-            <td></td>
-            <td>
-                <?=$partie = explode("(", $oiseau_protege["nom"])?>
-                <?=$partie[0]?>
-            </td>
-            <td>
-                <em><?=$label = str_replace(").",'',$partie[1])?><?=$label?></em>
-            </td>
-            <td><?=$oiseau_protege["id_article"]?></td>
-        </tr>
+        <?php foreach ($oiseaux_proteges as $oiseau_protege): ?>
+            <tr>
+                <td><?= $oiseau_protege["id"] ?></td>
+                <td></td>
+                <td>
+                    <?= $partie = explode("(", $oiseau_protege["nom"]) ?>
+                    <?= $partie[0] ?>
+                </td>
+                <td>
+                    <em><?= $label = str_replace(").", '', $partie[1]) ?><?= $label ?></em>
+                </td>
+                <td><?= $oiseau_protege["id_article"] ?></td>
+            </tr>
         <?php endforeach; ?>
     </tbody>
 </table>
