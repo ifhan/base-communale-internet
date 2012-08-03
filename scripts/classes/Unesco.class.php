5bbac5a60ab783f89d01c543adf87d15d7979dfc
Vignard <Ronan>
2012-08-03 12:07:18 +0200

    Formatage et doc

--- scripts/classes/Unesco.class.php
+++ scripts/classes/Unesco.class.php
@@ -9,26 +9,28 @@
  * @version 1.0
  */
 class Unesco {
-    public $id_regional;
-    public $id_bien;
-    public $nom;
-    public $url_site;
-    
-    public function getUnescoByIdRegional($id_regional){
+
+    /**
+     * Sélectionne un bien UNESCO par son identifiant régional
+     * @global string $pdo
+     * @param string $id_regional 
+     */
+    public function getUnescoByIdRegional($id_regional) {
         global $pdo;
         $sql = "SELECT * 
         FROM R_UNESCO_049
         WHERE id_regional = '$id_regional' ";
         try {
-            $unesco = $pdo->query($sql)->fetch();          
+            $unesco = $pdo->query($sql)->fetch();
             $this->id_regional = $unesco["id_regional"];
             $this->id_bien = $unesco["id_bien"];
             $this->nom = $unesco["nom"];
             $this->url_site = $unesco["url_site"];
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             echo 'ERROR: ' . $e->getMessage();
-        }    
+        }
     }
+
 }
 
 ?>
