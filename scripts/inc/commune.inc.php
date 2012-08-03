6791746c5219dd24cf2eb555aee3c3160d3d2344
Vignard <Ronan>
2012-08-03 11:46:56 +0200

    Formatage

--- scripts/inc/commune.inc.php
+++ scripts/inc/commune.inc.php
@@ -1,15 +1,15 @@
 <?php
 // Application-wide data and database connection
-require_once(dirname(__FILE__)."/../config/constants.inc.php");
-require_once(dirname(__FILE__)."/../config/database.inc.php");
+require_once(dirname(__FILE__) . "/../config/constants.inc.php");
+require_once(dirname(__FILE__) . "/../config/database.inc.php");
 
 // Utility functions  
-require_once (dirname(__FILE__)."/../classes/utilities.inc.php");
+require_once (dirname(__FILE__) . "/../classes/utilities.inc.php");
 
 // Classes
-require_once (dirname(__FILE__)."/../classes/Commune.class.php");
-require_once (dirname(__FILE__)."/../classes/Epci.class.php");
-require_once (dirname(__FILE__)."/../classes/Scot.class.php");
+require_once (dirname(__FILE__) . "/../classes/Commune.class.php");
+require_once (dirname(__FILE__) . "/../classes/Epci.class.php");
+require_once (dirname(__FILE__) . "/../classes/Scot.class.php");
 
 /**
  * @var $id_regional Identifiant régional du zonage
@@ -24,38 +24,38 @@ $id_commune = $_REQUEST["id_commune"];
 $id_epci = $_REQUEST["id_epci"];
 $id_scot = $_REQUEST["id_scot"];
 ?>
-<?php if(isset($id_regional)): ?>
-    <?php $communes = getCommunesByIdRegional($id_regional,$id_type); ?>
-<?php elseif(isset($id_epci)): ?>
+<?php if (isset($id_regional)): ?>
+    <?php $communes = getCommunesByIdRegional($id_regional, $id_type); ?>
+<?php elseif (isset($id_epci)): ?>
     <?php $communes = getCommunesEpciByIdEpci($id_epci); ?>
-<?php elseif(isset($id_scot)): ?>
+<?php elseif (isset($id_scot)): ?>
     <?php $communes = getCommunesScotByIdScot($id_scot); ?>
 <?php endif; ?>
-<?php if(isset($communes)): ?>
-<table width="99%">
-    <?php foreach($communes as $commune):?>
-    <tr>
-        <td bgcolor="<?=switchcolor()?>">
-        <?=$commune["id_commune"]?>
-        <?php
-        $str = $commune["id_commune"];
-        $str =  mb_strcut ($str,0,2);
-        
-        if ($str!=41 XOR $str!=61): 
-            echo "";
-        ?> 
-        <?php else: ?>
-            <a href="spip.php?page=fiche_commune&amp;id_commune=<?=$commune["id_commune"]?>">
-        <?php endif; ?>
-        <?=$commune["nom_commune"]?>
-            </a>
-        </td>
-    </tr>
-<?php endforeach; ?>
-</table>
+<?php if (isset($communes)): ?>
+    <table width="99%">
+        <?php foreach ($communes as $commune): ?>
+            <tr>
+                <td bgcolor="<?= switchcolor() ?>">
+                    <?= $commune["id_commune"] ?>
+                    <?php
+                    $str = $commune["id_commune"];
+                    $str = mb_strcut($str, 0, 2);
+
+                    if ($str != 41 XOR $str != 61):
+                        echo "";
+                        ?> 
+                    <?php else: ?>
+                        <a href="spip.php?page=fiche_commune&amp;id_commune=<?= $commune["id_commune"] ?>">
+                        <?php endif; ?>
+                        <?= $commune["nom_commune"] ?>
+                    </a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
 <?php endif; ?>
-<?php if(isset($id_commune)): ?>
+<?php if (isset($id_commune)): ?>
     <?php $commune = new Commune(); ?>
     <?php $commune->getCommuneById($id_commune); ?>
-    <?=$commune->nom_commune?> (<?=$commune->id_commune?>)
+    <?= $commune->nom_commune ?> (<?= $commune->id_commune ?>)
 <?php endif; ?>
