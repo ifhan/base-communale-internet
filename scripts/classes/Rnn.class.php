ea452dd407f3e8fca47a81f58fc3a4155c706719
Vignard <Ronan>
2012-08-03 12:00:29 +0200

    Formatage

--- scripts/classes/Rnn.class.php
+++ scripts/classes/Rnn.class.php
@@ -8,7 +8,7 @@
  * @version 1.0
  */
 class Rnn {
-    
+
     /**
      * Sélectionne une RNN par son identifiant régional
      * @global string $pdo
@@ -21,23 +21,24 @@ class Rnn {
         WHERE R_RNN_R52.id_regional = $id_regional 
         AND R_RNN_R52.id_regional = R_RNN_R52_data.id_regional ";
         try {
-            $row = $pdo->query($sql)->fetch();          
+            $row = $pdo->query($sql)->fetch();
             $this->id_regional = $row["id_regional"];
             $this->id_national = $row["id_national"];
             $this->nom = $row["nom"];
             $this->surf_sig_l93 = $row["surf_sig_l93"];
-            $this->id_decret  = $row['id_decret'];
-            $this->date  = date("d/m/Y", strtotime($row['date']));
-            $this->commentaire_decret  = $row['commentaire_decret'];
-            $this->url_decret  = $row['url_decret'];
-            $this->parcelles  = nl2br($row['parcelles']);
-            $this->statut_foncier  = nl2br($row['statut_foncier']);
-            $this->interet_bio  = nl2br($row['interet_bio']);
-            $this->effets_protection  = nl2br($row['effets_protection']);
-        } catch(PDOException $e) {
+            $this->id_decret = $row['id_decret'];
+            $this->date = date("d/m/Y", strtotime($row['date']));
+            $this->commentaire_decret = $row['commentaire_decret'];
+            $this->url_decret = $row['url_decret'];
+            $this->parcelles = nl2br($row['parcelles']);
+            $this->statut_foncier = nl2br($row['statut_foncier']);
+            $this->interet_bio = nl2br($row['interet_bio']);
+            $this->effets_protection = nl2br($row['effets_protection']);
+        } catch (PDOException $e) {
             echo 'ERROR: ' . $e->getMessage();
-        }   
+        }
     }
+
 }
 
 /**
@@ -59,8 +60,9 @@ function getRnnPhotosByIdRegional($id_regional) {
         $this->titre = $rnn_photos["titre"];
         $this->auteur = $rnn_photos["auteur"];
         $this->fournisseur = $rnn_photos["fournisseur"];
-    } catch(PDOException $e) {
+    } catch (PDOException $e) {
         echo 'ERROR: ' . $e->getMessage();
     }
 }
+
 ?>
