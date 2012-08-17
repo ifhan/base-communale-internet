346d5e4dc868a7ff317dfe1f0e0b9443c7686447
Vignard Ronan <>
2012-08-17 10:26:46 +0200

    Réécriture en OOP, formatage et doc

--- scripts/liste_scot.php
+++ scripts/liste_scot.php
@@ -2,106 +2,57 @@
 // Application-wide data and database connection
 require_once 'config/constants.inc.php';
 require_once 'config/database.inc.php';
-    
-/* On récupère si elle existe la valeur du département envoyée par le formulaire */
 
-$idr = isset($_POST['departement_scot']) ? $_POST['departement_scot'] : null;
-if (isset($_POST['ok']) && isset($_POST['scot']) && $_POST['scot'] != "") {
+// Classes
+require_once 'classes/Scot.class.php';
+require_once 'classes/Departement.class.php';
 
-    $departement_selectionne = $_POST['departement'];
-    $scot_selectionnee = $_POST['scot'];
-}
+$departements = getDepartementByRegion();
 
-/* On établit la connexion à MySQL avec mysql_pconnect() plutôt qu'avec mysql_connect()
-  car on aura besoin de la connexion un peu plus loin dans le script */
-
-$connexion = mysql_pconnect($host, $user, $pass);
-
-$choixbase = mysql_select_db($bdd, $connexion);
-$query_1 = "SELECT * FROM BDC_DEPARTEMENT_52 WHERE id_region = 18 ORDER BY nom_departement";
-$rech_departement = mysql_query($query_1);
-$code_departement = array();
-$nom_departement = array();
-
-/* On active un compteur pour les départements */
-
-$nb_departement = 0;
-if ($rech_departement != false) {
-
-    while ($ligne_departement = mysql_fetch_assoc($rech_departement)) {
-
-        array_push($code_departement, $ligne_departement['id_departement']);
-        array_push($nom_departement, utf8_encode($ligne_departement['nom_departement']));
-
-        /* On incrémente le compteur */
-
-        $nb_departement++;
-    }
-}
+/* Récupération de la valeur de l'identifiant du département envoyé 
+ * par le formulaire s'il existe */
+$id_dpt = isset($_POST['departement_scot'])?$_POST['departement_scot']:null;
 ?>
-<table><tr><td>
-<div class="select">
-	<form action="spip.php?rubrique2" method="post" id="changescot">
-	<p>D&eacute;partement&nbsp;:</p>	
-		<select name="departement_scot" id="departement_scot" onchange="document.forms['changescot'].submit();">
-			<option value="-1">---Choisir un d&eacute;partement---</option>
-<?php for ($i = 0; $i < $nb_departement; $i++) { ?>
-    			<option value="<?php echo($code_departement[$i]); ?>"<?php echo((isset($idr) && $idr == $code_departement[$i]) ? " selected=\"selected\"" : null); ?>><?php echo($nom_departement[$i]); ?></option>
-<?php } ?>
-		</select>
-<?php
-mysql_free_result($rech_departement);
-
-/* On commence par vérifier si on a envoyé un numéro de département et le cas échéant s'il est différent de -1 */
-
-if (isset($idr) && $idr != -1) {
-
-    /* Création de la requête pour avoir les scots de ce département */
-
-    $query_2 = "SELECT id_scot, nom_scot FROM R_SCOT_R52
-		WHERE id_departement = " . $idr . " ORDER BY nom_scot";
-
-    if ($connexion != false) {
-
-        $rech_scot = mysql_query($query_2, $connexion);
-
-        /* Un petit compteur pour les scots */
-
-        $nd = 0;
-
-        /* On crée deux tableaux pour les numéros et les noms des départements */
-
-        $code_scot = array();
-        $nom_scot = array();
-
-        /* On va mettre les numéros et noms des départements dans les deux tableaux */
-
-        while ($ligne_scot = mysql_fetch_assoc($rech_scot)) {
-
-            array_push($code_scot, $ligne_scot['id_scot']);
-            array_push($nom_scot, $ligne_scot['nom_scot']);
-            $nd++;
-        }
-?>
-        	</form>
-        </div>
-        </td><td>
-        <div class="select">
-        	<form action="spip.php?page=fiche_scot&amp;id_scot=" method="post">
-        	<p>SCoT&nbsp;:</p>
-        		<select name="id_scot" id="id_scot">
-        <?php for ($d = 0; $d < $nd; $d++) { ?>
-              			<option value="<?php echo($code_scot[$d]); ?>"<?php echo((isset($scot_selectionnee) && $scot_selectionnee == $code_scot[$d]) ? " selected=\"selected\"" : null); ?>><?php echo(utf8_encode($nom_scot[$d])); ?></option>
-        <?php } ?>
-        		</select>
-        	<input type="submit" name="ok" id="ok" value="Ok" />
+<table>
+    <tr>
+        <td>
+            <div class="select">
+                <form action="spip.php?rubrique2" method="post" id="changescot">
+                    <p>D&eacute;partement&nbsp;:</p>
+                    <select name="departement_scot" id="departement_scot" onchange="document.forms['changescot'].submit();">
+                        <option value="-1">---Choisir un d&eacute;partement---</option>
+                        <?php foreach($departements as $departement): ?>
+                        <option value="<?=$departement['id_departement']?>"
+                            <?php echo((isset($id_dpt) && $id_dpt == $departement['id_departement'])?" selected=\"selected\"":null); ?>>
+                            <?=$departement['nom_departement']?>
+                        </option>
+                        <?php endforeach; ?>
+                    </select>
+                </form>
+            </div>
+        </td>
         <?php
-    }
-
-    mysql_free_result($rech_scot);
-}
+        /* Vérification de l'instanciation de l'identifiant du département et 
+         * s'il est différent de -1 */
+        if (isset($id_dpt) && $id_dpt != -1):
         ?>
-	</form>
-</div>
-</td></tr></table>
-<?php mysql_close($connexion); ?>
+        <td>
+            <?php $array_scot = getScotsByIdDpt($id_dpt); ?>
+            <div class="select">
+                <form action="spip.php?page=fiche_scot&amp;id_scot=" method="post">
+                    <p>SCoT&nbsp;:</p>
+                    <select name="id_scot" id="id_scot">
+                        <?php foreach($array_scot as $scot): ?>
+                        <option value="<?= $scot['id_scot'] ?>">
+                        <?=$scot['nom_scot']?>
+                        </option>
+                        <?php endforeach; ?>
+                    </select>
+                    <input type="submit" name="ok" id="ok" value="Ok" />
+                </form>
+            </div>
+            <?php $pdo = null; ?>
+        </td>
+        <?php endif; ?>
+    </tr>
+</table>
