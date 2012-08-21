1f73dcbe45c2af2e88174669ae8ba6b1f6ef4ebc
Vignard Ronan <>
2012-08-21 17:29:18 +0200

    Modification des variables id_station en id_regional et nom_riviere en nom pour la table R_STATION_QUALITE_RCS_R52

--- scripts/inc/station_qualite.inc.php
+++ scripts/inc/station_qualite.inc.php
@@ -9,14 +9,14 @@ require_once(dirname(__FILE__)."/../classes/StationQualite.class.php");
 /**
  * Ce fichier sert à afficher le nom de la station sélectionnée dans un 
  * squelette SPIP
- * @var $id_station Identifiant de la station
+ * @var $id_regional Identifiant de la station
  */ 
-$id_station = $_REQUEST["id_station"];
+$id_regional = $_REQUEST["id_regional"];
 
 $station_qualite = new StationQualite();
-$station_qualite->getStationQualiteByIdStation($id_station);
+$station_qualite->getStationQualiteByIdStation($id_regional);
 ?>
-<?=$station_qualite->nom_riviere?> 
+<?=$station_qualite->nom?> 
 <?=$station_qualite->nom_commune?> 
 (<?=$station_qualite->id_commune?>), 
 <?=$station_qualite->localite?>
