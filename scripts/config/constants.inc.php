fb9d79ef0e891087f6cec38e6be026924541b5ba
Vignard Ronan <>
2012-08-22 14:31:19 +0200

    Formatage

--- scripts/config/constants.inc.php
+++ scripts/config/constants.inc.php
@@ -1,16 +1,22 @@
 <?php
-// Connection parameters
+/**
+ * Connection parameters
+ */
 define('USERNAME', 'root');
 define('PASSWORD', '%+carantec29');
 
-// URL parameters    
-define ('URL_BASE_COMMUNALE', 'http://www.donnees.pays-de-la-loire.developpement-durable.gouv.fr/');
+/**
+ * URL parameters
+ */
+define('URL_BASE_COMMUNALE', 
+        'http://www.donnees.pays-de-la-loire.developpement-durable.gouv.fr/');
 define('URL_CARMEN', 'http://carmen.developpement-durable.gouv.fr/26/');
-define('URL_DREAL', 'http://www.pays-de-la-loire.developpement-durable.gouv.fr/article.php3?id_article=');
+define('URL_DREAL', 
+        'http://www.pays-de-la-loire.developpement-durable.gouv.fr/article.php3?id_article=');
 define('URL_INPN_ESPACE_PROTEGE', 'http://inpn.mnhn.fr/espace/protege/');
 define('URL_INPN_ESPECE', 'http://inpn.mnhn.fr/isb/espece/cd_nom/');
 define('URL_INPN_NATURA_2000', 'http://inpn.mnhn.fr/isb/site/natura2000/');
 define('URL_INPN_ZNIEFF', 'http://inpn.mnhn.fr/isb/zone/znieff/');
 define('URL_PRIM_NET', 'http://macommune.prim.net/d_commune.php?insee=');
-define('URL_INSEE', 'http://www.statistiques-locales.insee.fr/esl/default.asp?page=statistiques-locales/chiffres-cles/recherche-zonage/choix-pdf&IdSelGeo=');
-?>
+define('URL_INSEE', 
+        'http://www.statistiques-locales.insee.fr/esl/default.asp?page=statistiques-locales/chiffres-cles/recherche-zonage/choix-pdf&IdSelGeo=');
