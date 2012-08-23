78163a9acde64242266119c5f5c41138852c2fa7
Vignard Ronan <>
2012-08-23 11:20:06 +0200

    Modification de la requête (utilisation de exec() à la place de query() et documentation

--- scripts/classes/Docob.class.php
+++ scripts/classes/Docob.class.php
@@ -10,8 +10,9 @@
 class Docob {
     
     /**
-     * 
-     * @global string $pdo
+     * Sélectionne l'ensemble des sites Natura 2000 pour afficher 
+     * un tableau des DOCOB
+     * @global string $pdo Connexion à la base de données
      * @return array 
      */
     public function getDocob() {
@@ -38,18 +39,21 @@ class Docob {
 }
 
 /**
- * 
- * @global string $pdo
+ * Sélectionne l'ensemble des sites Natura 2000 pour afficher 
+ * un tableau des DOCOB
+ * @global string $pdo Connexion à la base de données
  * @return array 
  */
 function getDocob() {
     global $pdo;
-    $sql = "(SELECT * FROM R_ZPS_R52_data) 
-            UNION  (SELECT * FROM R_ZSC_R52_data) 
-            UNION(SELECT * FROM R_SIC_R52_data) 
-            ORDER BY id_regional";
+    $sql = $pdo->prepare('(SELECT * 
+        FROM R_ZPS_R52_data) 
+        UNION  (SELECT * FROM R_ZSC_R52_data) 
+        UNION(SELECT * FROM R_SIC_R52_data) 
+        ORDER BY id_regional');
+    $sql->execute();
     try {
-        $docob = $pdo->query($sql)->fetchAll();
+        $docob = $sql->fetchAll();
         return $docob;
         $this->id_regional = $docob["id_regional"];
         $this->nom = $docob["nom"];
