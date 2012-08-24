575e16d5d7590b2a9a610dff16ab80bb9cef0c0f
Vignard Ronan <>
2012-08-24 11:01:03 +0200

    Modification des requêtes query() en exec()

--- scripts/classes/EspeceDeterminante.class.php
+++ scripts/classes/EspeceDeterminante.class.php
@@ -8,26 +8,25 @@
  * @version 1.0
  */
 class EspeceDeterminante {
-    //put your code here
+
 }
 
 /**
  * Sélectionne les espèces déterminantes pour la flore en Pays de la Loire
- * @global string $pdo
+ * @global string $pdo Connexion à la base de données
  * @return array 
  */
 function getEspecesDeterminantesFlore() {
     global $pdo;
-    $table = "R_ESPECES_DETERMINANTES_FLORE_R52";
-    $table_2 = "R_ESPECES_DETERMINANTES_ZNIEFF_R52";
-
-    $sql = "SELECT * 
-    FROM $table, $table_2 
-    WHERE $table.ID = $table_2.ID 
+    $sql = $pdo->prepare('SELECT * 
+    FROM R_ESPECES_DETERMINANTES_FLORE_R52, $table_2 
+    WHERE R_ESPECES_DETERMINANTES_FLORE_R52.ID = 
+    R_ESPECES_DETERMINANTES_ZNIEFF_R52.ID 
     GROUP BY GENRE 
-    ORDER BY GENRE ";
+    ORDER BY GENRE');
+    $sql->execute();
     try {
-        $especes_determinantes_flore = $pdo->query($sql)->fetchAll();
+        $especes_determinantes_flore = $sql->fetchAll();
         return $especes_determinantes_flore;
     } catch (PDOException $e) {
         echo 'ERROR: ' . $e->getMessage();
@@ -36,20 +35,18 @@ function getEspecesDeterminantesFlore() {
 
 /**
  * Sélectionne les espèces déterminantes pour la faune en Pays de la Loire
- * @global string $pdo
+ * @global string $pdo Connexion à la base de données
  * @return array 
  */
 function getEspecesDeterminantesFaune() {
     global $pdo;
-    $sql = "SELECT * 
-    FROM R_ESPECES_DETERMINANTES_FAUNE_R52
-    ORDER BY GENRE";
+    $sql = $pdo->prepare('SELECT * FROM R_ESPECES_DETERMINANTES_FAUNE_R52
+    ORDER BY GENRE');
+    $sql->execute();
     try {
-        $especes_determinantes_faune = $pdo->query($sql)->fetchAll();
+        $especes_determinantes_faune = $sql->fetchAll();
         return $especes_determinantes_faune;
     } catch (PDOException $e) {
         echo 'ERROR: ' . $e->getMessage();
     }
 }
-
-?>
