bd7730240398aa8a10646b9bf6292d9ab2fa3d9b
Vignard Ronan <>
2012-08-27 10:56:14 +0200

    Modification de query() en exec() dans les requêtes et documentation

--- scripts/classes/HabitatCorine.class.php
+++ scripts/classes/HabitatCorine.class.php
@@ -10,34 +10,17 @@
 class HabitatCorine {
 
     /**
-     * Sélection de l'ensemble des habitats ZNIEFF (nomenclature CORINE)
-     * @global type $pdo Connexion à la base de données
-     * @return array 
-     */
-    public function getHabitatsCorine() {
-        $pdo = ConnectionFactory::getFactory()->getConnection();
-        $sql = "SELECT * 
-        FROM natura_eur15";
-        try {
-            $habitats_corine = $pdo->query($sql)->fetchAll();
-            return $habitats_corine;
-        } catch (PDOException $e) {
-            echo 'ERROR: ' . $e->getMessage();
-        }
-    }
-
-    /**
      * Sélection d'un habitat par son identifiant
-     * @global type $pdo Connexion à la base de données
      * @param string $id_corine Identifiant CORINE de l'habitat
      */
     public function getHabitatCorineById($id_corine) {
         $pdo = ConnectionFactory::getFactory()->getConnection();
-        $sql = "SELECT * 
-        FROM znieff_typologie 
-        WHERE CD_TYPO = '$id_corine' ";
+        $sql = $pdo->prepare('SELECT * FROM znieff_typologie 
+        WHERE CD_TYPO = :id_corine');
+        $sql->bindParam(':id_corine', $id_corine, PDO::PARAM_STR, 5);
+        $sql->execute();
         try {
-            $row = $pdo->query($sql)->fetch();
+            $row = $sql->fetch();
             $this->id_corine = $row['CD_TYPO'];
             $this->lb_corine = $row['LB_TYPO'];
         } catch (PDOException $e) {
@@ -49,15 +32,14 @@ class HabitatCorine {
 
 /**
  * Sélection de l'ensemble des habitats ZNIEFF (nomenclature CORINE)
- * @global type $pdo Connexion à la base de données
  * @return array 
  */
 function getHabitatsCorine() {
     $pdo = ConnectionFactory::getFactory()->getConnection();
-    $sql = "SELECT * 
-        FROM znieff_typologie ";
+    $sql = $pdo->prepare('SELECT * FROM znieff_typologie');
+    $sql->execute();
     try {
-        $habitats_corine = $pdo->query($sql)->fetchAll();
+        $habitats_corine = $sql->fetchAll();
         return $habitats_corine;
     } catch (PDOException $e) {
         echo 'ERROR: ' . $e->getMessage();
@@ -66,16 +48,15 @@ function getHabitatsCorine() {
 
 /**
  * Récupère les habitats CORINE du thème 1
- * @global type $pdo Connexion à la base de données
  * @return array 
  */
 function getHabitatsCorineByCdTypo1() {
     $pdo = ConnectionFactory::getFactory()->getConnection();
-    $sql = "SELECT * 
-        FROM znieff_typologie 
-        WHERE CD_TYPO LIKE '1%' ";
+    $sql = $pdo->prepare('SELECT * FROM znieff_typologie 
+    WHERE CD_TYPO LIKE 1%');
+    $sql->execute();
     try {
-        $habitats_corine_1 = $pdo->query($sql)->fetchAll();
+        $habitats_corine_1 = $sql->fetchAll();
         return $habitats_corine_1;
     } catch (PDOException $e) {
         echo 'ERROR: ' . $e->getMessage();
@@ -84,16 +65,15 @@ function getHabitatsCorineByCdTypo1() {
 
 /**
  * Récupère les habitats CORINE du thème 2
- * @global type $pdo Connexion à la base de données
  * @return array 
  */
 function getHabitatsCorineByCdTypo2() {
     $pdo = ConnectionFactory::getFactory()->getConnection();
-    $sql = "SELECT * 
-        FROM znieff_typologie 
-        WHERE CD_TYPO LIKE '2%' ";
+    $sql = $pdo->prepare('SELECT * FROM znieff_typologie 
+    WHERE CD_TYPO LIKE 2%');
+    $sql->execute();
     try {
-        $habitats_corine_2 = $pdo->query($sql)->fetchAll();
+        $habitats_corine_2 = $sql->fetchAll();
         return $habitats_corine_2;
     } catch (PDOException $e) {
         echo 'ERROR: ' . $e->getMessage();
@@ -102,16 +82,15 @@ function getHabitatsCorineByCdTypo2() {
 
 /**
  * Récupère les habitats CORINE du thème 3
- * @global type $pdo Connexion à la base de données
  * @return array 
  */
 function getHabitatsCorineByCdTypo3() {
     $pdo = ConnectionFactory::getFactory()->getConnection();
-    $sql = "SELECT * 
-        FROM znieff_typologie 
-        WHERE CD_TYPO LIKE '3%' ";
+    $sql = $pdo->prepare('SELECT * FROM znieff_typologie 
+    WHERE CD_TYPO LIKE 3%');
+    $sql->execute();
     try {
-        $habitats_corine_3 = $pdo->query($sql)->fetchAll();
+        $habitats_corine_3 = $sql->fetchAll();
         return $habitats_corine_3;
     } catch (PDOException $e) {
         echo 'ERROR: ' . $e->getMessage();
@@ -120,16 +99,15 @@ function getHabitatsCorineByCdTypo3() {
 
 /**
  * Récupère les habitats CORINE du thème 4
- * @global type $pdo Connexion à la base de données
  * @return array 
  */
 function getHabitatsCorineByCdTypo4() {
     $pdo = ConnectionFactory::getFactory()->getConnection();
-    $sql = "SELECT * 
-        FROM znieff_typologie 
-        WHERE CD_TYPO LIKE '4%' ";
+    $sql = $pdo->prepare('SELECT * FROM znieff_typologie 
+    WHERE CD_TYPO LIKE 4%');
+    $sql->execute();
     try {
-        $habitats_corine_4 = $pdo->query($sql)->fetchAll();
+        $habitats_corine_4 = $sql->fetchAll();
         return $habitats_corine_4;
     } catch (PDOException $e) {
         echo 'ERROR: ' . $e->getMessage();
@@ -138,16 +116,15 @@ function getHabitatsCorineByCdTypo4() {
 
 /**
  * Récupère les habitats CORINE du thème 5
- * @global type $pdo Connexion à la base de données
  * @return array 
  */
 function getHabitatsCorineByCdTypo5() {
     $pdo = ConnectionFactory::getFactory()->getConnection();
-    $sql = "SELECT * 
-        FROM znieff_typologie 
-        WHERE CD_TYPO LIKE '5%' ";
+    $sql = $pdo->prepare('SELECT * FROM znieff_typologie 
+    WHERE CD_TYPO LIKE 5%');
+    $sql->execute();
     try {
-        $habitats_corine_5 = $pdo->query($sql)->fetchAll();
+        $habitats_corine_5 = $sql->fetchAll();
         return $habitats_corine_5;
     } catch (PDOException $e) {
         echo 'ERROR: ' . $e->getMessage();
@@ -156,16 +133,15 @@ function getHabitatsCorineByCdTypo5() {
 
 /**
  * Récupère les habitats CORINE du thème 6
- * @global type $pdo Connexion à la base de données
  * @return array 
  */
 function getHabitatsCorineByCdTypo6() {
     $pdo = ConnectionFactory::getFactory()->getConnection();
-    $sql = "SELECT * 
-        FROM znieff_typologie 
-        WHERE CD_TYPO LIKE '6%' ";
+    $sql = $pdo->prepare('SELECT * FROM znieff_typologie 
+    WHERE CD_TYPO LIKE 6%');
+    $sql->execute();
     try {
-        $habitats_corine_6 = $pdo->query($sql)->fetchAll();
+        $habitats_corine_6 = $sql->fetchAll();
         return $habitats_corine_6;
     } catch (PDOException $e) {
         echo 'ERROR: ' . $e->getMessage();
@@ -174,16 +150,15 @@ function getHabitatsCorineByCdTypo6() {
 
 /**
  * Récupère les habitats CORINE du thème 7
- * @global type $pdo Connexion à la base de données
  * @return array 
  */
 function getHabitatsCorineByCdTypo7() {
     $pdo = ConnectionFactory::getFactory()->getConnection();
-    $sql = "SELECT * 
-        FROM znieff_typologie 
-        WHERE CD_TYPO LIKE '7%' ";
+    $sql = $pdo->prepare('SELECT * FROM znieff_typologie 
+    WHERE CD_TYPO LIKE 7%');
+    $sql->execute();
     try {
-        $habitats_corine_7 = $pdo->query($sql)->fetchAll();
+        $habitats_corine_7 = $sql->fetchAll();
         return $habitats_corine_7;
     } catch (PDOException $e) {
         echo 'ERROR: ' . $e->getMessage();
@@ -192,16 +167,15 @@ function getHabitatsCorineByCdTypo7() {
 
 /**
  * Récupère les habitats CORINE du thème 8
- * @global type $pdo Connexion à la base de données
  * @return array 
  */
 function getHabitatsCorineByCdTypo8() {
     $pdo = ConnectionFactory::getFactory()->getConnection();
-    $sql = "SELECT * 
-        FROM znieff_typologie 
-        WHERE CD_TYPO LIKE '8%' ";
+    $sql = $pdo->prepare('SELECT * FROM znieff_typologie 
+    WHERE CD_TYPO LIKE 8%');
+    $sql->execute();
     try {
-        $habitats_corine_8 = $pdo->query($sql)->fetchAll();
+        $habitats_corine_8 = $sql->fetchAll();
         return $habitats_corine_8;
     } catch (PDOException $e) {
         echo 'ERROR: ' . $e->getMessage();
