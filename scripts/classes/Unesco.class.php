e9850bd074840c1951b3b730804bd664fd945980
Vignard Ronan <>
2012-08-27 10:19:14 +0200

    Modification de query() en exec() dans les requêtes, documentation et supression du tag PHP fermant

--- scripts/classes/Unesco.class.php
+++ scripts/classes/Unesco.class.php
@@ -12,25 +12,24 @@ class Unesco {
 
     /**
      * Sélectionne un bien UNESCO par son identifiant régional
-     * @global string $pdo
-     * @param string $id_regional 
+     * @global string $pdo Connexion à la base de données
+     * @param string $id_regional Identifiant régional du zonage
      */
     public function getUnescoByIdRegional($id_regional) {
         $pdo = ConnectionFactory::getFactory()->getConnection();
-        $sql = "SELECT * 
-        FROM R_UNESCO_049
-        WHERE id_regional = '$id_regional' ";
+        $sql = $pdo->prepare('SELECT * FROM R_UNESCO_049
+        WHERE id_regional = :id_regional');
+        $sql->bindParam(':id_regional', $id_regional, PDO::PARAM_STR, 5);
+        $sql->execute();
         try {
-            $unesco = $pdo->query($sql)->fetch();
-            $this->id_regional = $unesco["id_regional"];
-            $this->id_bien = $unesco["id_bien"];
-            $this->nom = $unesco["nom"];
-            $this->url_site = $unesco["url_site"];
+            $row = $sql->fetch();
+            $this->id_regional = $row["id_regional"];
+            $this->id_bien = $row["id_bien"];
+            $this->nom = $row["nom"];
+            $this->url_site = $row["url_site"];
         } catch (PDOException $e) {
             echo 'ERROR: ' . $e->getMessage();
         }
     }
 
 }
-
-?>
