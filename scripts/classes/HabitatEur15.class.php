48bb33a2a97d29e9e54247ca6771c1c3e78b286d
Vignard Ronan <>
2012-08-27 10:50:18 +0200

    Modification de query() en exec() dans les requêtes, documentation et supression du tag PHP fermant

--- scripts/classes/HabitatEur15.class.php
+++ scripts/classes/HabitatEur15.class.php
@@ -10,36 +10,17 @@
 class HabitatEur15 {
 
     /**
-     * Sélection de l'ensemble de la liste des habitats Natura 2000 
-     * (nomenclature EUR15)
-     * @global string $pdo Connexion à la base de données
-     * @return array 
-     */
-    public function getHabitatsEur15() {
-        $pdo = ConnectionFactory::getFactory()->getConnection();
-        $sql = "SELECT * 
-        FROM natura_eur15";
-        try {
-            $habitats_eur15 = $pdo->query($sql)->fetchAll();
-            return $habitats_eur15;
-        } catch (PDOException $e) {
-            echo 'ERROR: ' . $e->getMessage();
-        }
-    }
-
-    /**
      * Sélection d'un habitat par son identifiant
-     * @global string $pdo Connexion à la base de données
      * @param int $id_eur15 Identifiant EUR15 de l'habitat
      */
     public function getHabitatEur15ById($id_eur15) {
         $pdo = ConnectionFactory::getFactory()->getConnection();
-        $sql = "SELECT * 
-        FROM natura_eur15 
-        WHERE ID_EUR15 = '$id_eur15' ";
+        $sql = $pdo->prepare('SELECT * FROM natura_eur15 
+        WHERE ID_EUR15 = :id_eur15');
+        $sql->bindParam(':id_eur15', $id_eur15, PDO::PARAM_STR, 11);
+        $sql->execute();
         try {
-            $row = $pdo->query($sql)->fetch();
-
+            $row = $sql->fetch();
             $this->id_eur15 = $row['ID_EUR15'];
             $this->lb_eur15 = $row['LB_EUR15'];
         } catch (PDOException $e) {
@@ -57,10 +38,10 @@ class HabitatEur15 {
  */
 function getHabitatsEur15() {
     $pdo = ConnectionFactory::getFactory()->getConnection();
-    $sql = "SELECT * 
-        FROM natura_eur15";
+    $sql = $pdo->prepare('SELECT * FROM natura_eur15');
+    $sql->execute();
     try {
-        $habitats_eur15 = $pdo->query($sql)->fetchAll();
+        $habitats_eur15 = $sql->fetchAll();
         return $habitats_eur15;
     } catch (PDOException $e) {
         echo 'ERROR: ' . $e->getMessage();
