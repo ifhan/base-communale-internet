9c5a2df0ed22e82d9a9848a91cb058ee94490d74
Ronan Vignard <ronan.vignard@yahoo.fr>
2012-09-05 16:48:41 +0200

    Création du fichier

--- scripts/fiche_zde.php
+++ scripts/fiche_zde.php
@@ -4,7 +4,7 @@ require_once 'config/constants.inc.php';
 require_once 'config/database.inc.php';
 
 // Classes
-require_once 'classes/Apb.class.php';
+require_once 'classes/Zde.class.php';
 require_once 'classes/Departement.class.php';
 
 /**
@@ -12,44 +12,45 @@ require_once 'classes/Departement.class.php';
  * @var $id_regional Identifiant régional du zonage
  */
 $id_type = $_REQUEST["id_type"];
-$id_regional = $_REQUEST["id_regional"];
-$apb = new Apb();
-$apb->getApbByIdRegional($id_regional);
+$id_zde = $_REQUEST["id_zde"];
+$zde = new Zde();
+$zde->getZdeByIdZde($id_zde);
 
-$departement = new Departement();
-$departement->getDepartementByIdRegional($id_regional, $id_type);
+/*$departement = new Departement();
+$departement->getDepartementByIdRegional($id_regional, $id_type);*/
 ?>
 <table class="cadre_plein">
     <tr>
         <td>Nom&nbsp;:</td>
-        <td><strong><?=$apb->nom?></strong></td>
+        <td><strong><?=$zde->nom_zde?></strong></td>
     </tr>
     <tr>
-        <td>Identifiant r&eacute;gional&nbsp;:</td>
-        <td><strong><?=$apb->id_regional?></strong></td>
+        <td>Identifiant&nbsp;:</td>
+        <td><strong><?=$zde->id_zde?></strong></td>
     </tr>
     <tr>
-        <td>Identifiant SPN&nbsp;: </td>
-        <td><strong><?=$apb->id_national?></strong></td>
+        <td>&Eacute;tat d'avancement de l'instruction de la demande de ZDE &nbsp;:</td>
+        <td><strong><?=$zde->etat_zde?></strong></td>
     </tr>
     <tr>
-        <td>Arr&ecirc;t&eacute;&nbsp;:</td>
+        <td>Surface d&eacute;clar&eacute;e par le demandeur &nbsp;:</td>
+        <td><strong><?=$zde->surfdeclar?> ha</strong></td>
+    </tr>
+    <tr>
+        <td>Date de l'arr&ecirc;t&eacute;&nbsp;: </td>
+        <td><strong><?=$zde->datearrete?></strong></td>
+    </tr>
+    <tr>
+        <td>R&eacute;f&eacute;rence de l'arr&ecirc;t&eacute;&nbsp;:</td>
         <td>
-            <strong>
-                n&deg; <?=$apb->code_arrete?>
-                du <?=$apb->date?>
-            </strong>
+            <strong><?=$zde->refarrete?></strong>
         </td>
     </tr>
     <tr>
-        <td></td>
+        <td>Puissances minimale/maximale</td>
         <td>
-            <em><?=$apb->commentaire_arrete?></em>
+            <strong><?=$zde->pu_mini?> MW/<?=$zde->pu_maxi?> MW</strong>
         </td>
-    </tr>
-    <tr>
-        <td>Surface calcul&eacute;e dans le SIG&nbsp;: </td>
-        <td><strong><?=$apb->surf_sig_l93?> ha</strong></td>
     </tr>
     <tr>
         <td>D&eacute;partement&nbsp;: </td>
@@ -62,19 +63,19 @@ $departement->getDepartementByIdRegional($id_regional, $id_type);
 </table>
 <h3 class="spip">Commune(s) concern&eacute;e(s)&nbsp;:</h3>
 <?php require_once 'inc/commune.inc.php'; ?>
-<?php if(isset($apb->parcelles)): ?>
+<?php if(isset($zde->parcelles)): ?>
 <h3 class="spip">Parcelles cadastrales&nbsp;:</h3>
 <?php endif; ?>
-<p><?=$apb->parcelles?></p>
-<?php if(isset($apb->statut_foncier)): ?>
+<p><?=$zde->parcelles?></p>
+<?php if(isset($zde->statut_foncier)): ?>
 <h3 class="spip">Statut foncier&nbsp;:</h3>
-<p><?=$apb->statut_foncier?></p>
+<p><?=$zde->statut_foncier?></p>
 <?php endif; ?>
-<?php if(isset($apb->interet_bio)): ?>
+<?php if(isset($zde->interet_bio)): ?>
 <h3 class="spip">Int&eacute;r&ecirc;t biologique&nbsp;:</h3>
-<p><?=$apb->interet_bio?></p>
+<p><?=$zde->interet_bio?></p>
 <?php endif; ?>
-<?php if(isset($apb->effets_protection)): ?>
+<?php if(isset($zde->effets_protection)): ?>
 <h3 class="spip">Effets de la protection&nbsp;:</h3>
-<p><?=$apb->effets_protection?></p>
+<p><?=$zde->effets_protection?></p>
 <?php endif; ?>
