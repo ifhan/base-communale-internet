cc2818b2d6becd891ae5cb60a7b2748ca2d3b8fd
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-08-22 09:16:37 +0200

    Modification du connecteur PDO

--- scripts/config/database.inc.php
+++ scripts/config/database.inc.php
@@ -1,10 +1,21 @@
 <?php
-// Database connection using PDO
+// Database connection using PDO with MySQL
 try {
     $pdo = new PDO(
-                    'mysql:host=localhost;dbname=base_communale_internet', USERNAME, PASSWORD);
+                    'mysql:host=localhost;dbname=base_communale_internet', 
+            USERNAME, PASSWORD);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     $pdo->exec("SET CHARACTER SET utf8");
+} catch (PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
+
+// Database connection using PDO with PostGreSQL
+try {
+    $pdo_pg = new PDO(
+                    'pgsql:host=10.44.128.174;dbname=bd_eolien', 
+            'dreal', 'dreal');
+    $pdo_pg->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 } catch (PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
 }
