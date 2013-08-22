9dacc1e20c88a63f6168ec976f58f56d0ae8c176
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-08-22 09:16:17 +0200

    Ajout d'une URL en constante

--- scripts/config/constants.inc.php
+++ scripts/config/constants.inc.php
@@ -13,6 +13,8 @@ define('URL_BASE_COMMUNALE',
 define('URL_CARMEN', 'http://carmen.developpement-durable.gouv.fr/26/');
 define('URL_DREAL', 
         'http://www.pays-de-la-loire.developpement-durable.gouv.fr/article.php3?id_article=');
+define('URL_DREAL_RUB', 
+        'http://www.pays-de-la-loire.developpement-durable.gouv.fr/rubrique.php3?id_rubrique=');
 define('URL_INPN_ESPACE_PROTEGE', 'http://inpn.mnhn.fr/espace/protege/');
 define('URL_INPN_ESPECE', 'http://inpn.mnhn.fr/isb/espece/cd_nom/');
 define('URL_INPN_NATURA_2000', 'http://inpn.mnhn.fr/isb/site/natura2000/');
