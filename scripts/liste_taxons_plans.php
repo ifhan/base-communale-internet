9e349660f31056f8ca8b4324de13cacf937e42f0
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-11-06 11:35:48 +0100

    First commit

--- scripts/liste_taxons_plans.php
+++ scripts/liste_taxons_plans.php
@@ -9,18 +9,56 @@ require_once 'classes/utilities.inc.php';
 // Classes
 require_once 'classes/TaxonsPlans.class.php';
 
-$taxons = getTaxonsPNA();
+/**
+ * @var $type_plan Type de plan
+ * @var $type_sp Type d'espèces (Faune/Flore)
+ */
+$type_plan = $_REQUEST["type_plan"];
+$type_sp = $_REQUEST["type_sp"];
+
+$taxons = getTaxons($type_plan,$type_sp);
 ?>
 <div class="listerub">
     <div class="titresousrub">Liens</div>
     <div id ="menu3">
         <div class="voir_sites">
             <ul>
+                <?php
+                switch ($type_plan):
+                    case "PNA":
+                ?>
                 <li>
                     <a href="http://www.developpement-durable.gouv.fr/-Especes-menacees-les-plans-.html" target="_blank">
                         Consulter la rubrique de pr&eacute;sentation des plans 
                         nationaux d'action sur le site du Minist&egrave;re</a>.
                 </li><br />
+                <?php
+                        break;
+                    case "PCR":
+                ?>
+                <li>
+                    <a href="http://www.cbnbrest.fr/site/html/regions/strategie_conservation.html" target="_blank">
+                        Consulter la strat&eacute;gie de conservation sur le site 
+                        du <abbr  title="Conservatoire Botanique National de Brest">CBNB</abbr></a>.
+                </li>
+                <li>
+                    <a href="http://www.cbnbrest.fr/site/html/regions/strategie_conservation_pdl.html#pla" target="_blank">
+                        Consulter les plans de conservations sur le site 
+                        du <abbr  title="Conservatoire Botanique National de Brest">CBNB</abbr></a>.
+                </li><br />                
+                <?php
+                        break;
+                    case "PCL":
+                ?>
+                <li>
+                    <a href="http://www.pays-de-la-loire.developpement-durable.gouv.fr/plans-de-conservation-issus-de-r642.html" target="_blank">
+                        Consulter la présentation sur le site Internet de la DREAL</a>.
+                </li><br />                
+                <?php
+                        break;
+                endswitch;
+                ?>
+
             </ul>
         </div>
     </div>
@@ -32,7 +70,9 @@ $taxons = getTaxonsPNA();
             <th>Nom scientifique</th>
             <th>Nom vernaculaire</th>
             <th>Fiche INPN</th>
+            <?php if($type_plan == "PNA"): ?>
             <th>Plan d'action</th>
+            <?php endif; ?>
     </thead>
     <tbody>
         <?php foreach ($taxons as $taxon): ?>
@@ -79,12 +119,14 @@ $taxons = getTaxonsPNA();
             <?php  else: ?>
             <td></td>
             <?php endif; ?>
+            <?php if($type_plan == "PNA"): ?>
             <td>
                 <a href="<?=$taxon["URL"]?>" target="_blank">
                     <img src="IMG/png/gnome-globe.png" style="border:none"  alt="T&eacute;l&eacute;charger" />
                     <strong>Consulter le plan d'actions</strong><br /> sur le 
                     site du Minist&egrave;re</a>.
             </td>
+            <?php endif; ?>
         </tr>
         <?php endforeach; ?>
     </tbody>
