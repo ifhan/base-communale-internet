8a31c3dff52ddae34ed150b7c339b9bb8c66855b
Ronan Vignard <ronan.vignard@yahoo.fr>
2013-11-06 11:33:27 +0100

    First commit

--- scripts/inc/type_espece.inc.php
+++ scripts/inc/type_espece.inc.php
@@ -3,30 +3,9 @@
 require_once(dirname(__FILE__)."/../config/constants.inc.php");
 require_once(dirname(__FILE__)."/../config/database.inc.php");
 
-// Classes
-require_once(dirname(__FILE__)."/../classes/Zonage.class.php");
-require_once(dirname(__FILE__)."/../classes/HabitatEur15.class.php");
-require_once(dirname(__FILE__)."/../classes/HabitatCorine.class.php");
+$type_sp  = $_REQUEST["type_sp"];
 
-$id_type = $_REQUEST["id_type"];
-$id_eur15 = $_REQUEST["id_eur15"];
-$id_corine = $_REQUEST["id_corine"];
-
-if(isset($id_type)) {
-    $zonage = new Zonage();
-    $zonage->getTypeZonageByIdType($id_type);
-    echo $zonage->type;
-}
-
-if(isset($id_eur15)) {
-    $habitat_eur15 = new HabitatEur15();
-    $habitat_eur15->getHabitatEur15ById($id_eur15);
-    echo $habitat_eur15->lb_eur15;
-}
-	
-if(isset($id_corine)) {
-    $habitat_corine = new HabitatCorine();
-    $habitat_corine->getHabitatCorineById($id_corine);
-    echo $habitat_corine->id_corine." - ".$habitat_corine->lb_corine;
+if(isset($type_sp)) {
+    echo ucfirst(strtolower($type_sp));
 }
 ?>
