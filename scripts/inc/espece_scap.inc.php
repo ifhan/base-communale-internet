9bbd87ae3abaf20fde98fbf3d83f7951a58a18ed
Ronan Vignard <ronan.vignard@yahoo.fr>
2014-05-13 16:52:52 +0200

    Modification du tableau

--- scripts/inc/espece_scap.inc.php
+++ scripts/inc/espece_scap.inc.php
@@ -22,15 +22,33 @@ if (isset($id_regional)):
     $sp_connues = getEspecesScapByIdRegional($id_regional);
 endif;
 if (isset($communes)): ?>
-    <table width="99%">
+    <table  class="spip">
+    <tr class="row_first">
+        <th>Identifiant TAXREF</th>
+        <th>Nom scientifique</th>
+        <th>Nom vernaculaire</th>
+        <th>Priorit&eacute; SCAP</th>
+        <th>Esp&egrave;ce &agrave; faible occurence ?</th>
+    </tr>
         <?php foreach ($sp_connues as $sp_connue): ?>
-            <tr>
-                <td bgcolor="<?=switchColor()?>">
+            <tr bgcolor="<?=switchColor()?>">
+                <td>
                     <?=$sp_connue["id_taxref"]?>
+                </td>
+                <td>
                     <a href="http://inpn.mnhn.fr/espece/cd_nom/<?=$sp_connue["id_taxref"]?>" 
                         target="_blank">
                         <em><?= $sp_connue["lb_nom"] ?></em>
                     </a>
+                </td>
+                <td><?= $sp_connue["nom_vernac"] ?></td>
+                <td><?= $sp_connue["priorite"] ?></td>
+                <td>
+                    <?php if($sp_connue["nb_occurence"] < "100"):
+                        echo "Oui";
+                    else:
+                        echo "Non";
+                    endif; ?>
                 </td>
             </tr>
         <?php endforeach; ?>
