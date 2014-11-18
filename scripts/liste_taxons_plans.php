bf52c28872558b7f6016f53cf68490291cbb9a20
Ronan Vignard <ronan.vignard@yahoo.fr>
2014-11-18 16:23:19 +0100

    Modification de la liste
    Affiche le nombre de résultats et les PRA.

--- scripts/liste_taxons_plans.php
+++ scripts/liste_taxons_plans.php
@@ -18,6 +18,12 @@ $type_sp = $_REQUEST["type_sp"];
 
 $taxons = getTaxons($type_plan,$type_sp);
 ?>
+<?php // Affiche le nombre de résultats retourne par la requête  ?>
+<p>
+    <strong><?= count($taxons) ?> taxon(s)</strong> 
+        recens&eacute;(s) pour le type de plan s&eacute;lectionn&eacute;.
+</p>
+<?php if (count($taxons) > 0): ?>
 <div class="listerub">
     <div class="titresousrub">Liens</div>
     <div id ="menu3">
@@ -45,6 +51,12 @@ $taxons = getTaxons($type_plan,$type_sp);
                     <a href="http://www.cbnbrest.fr/site/html/regions/strategie_conservation_pdl.html#pla" target="_blank">
                         Consulter les plans de conservations sur le site 
                         du <abbr  title="Conservatoire Botanique National de Brest">CBNB</abbr></a>.
+                </li> 
+                <li>
+                    <a href="data/docs/rapports/flore/RAPPORT_CBNB_ suivi des PCR _PDL_2013 tr.pdf" target="_blank">
+                    T&eacute;l&eacute;charger le rapport <abbr  title="Conservatoire Botanique National de Brest">CBNB</abbr>
+                    de suivi 2013 des Plans de Conservations R&eacute;gionaux</a> 
+                    (format PDF,&nbsp;<?= @convertFilesize("data/docs/rapports/flore/RAPPORT_CBNB_ suivi des PCR _PDL_2013 tr.pdf") ?>).
                 </li><br />                
                 <?php
                         break;
@@ -53,7 +65,15 @@ $taxons = getTaxons($type_plan,$type_sp);
                 <li>
                     <a href="http://www.pays-de-la-loire.developpement-durable.gouv.fr/plans-de-conservation-issus-de-r642.html" target="_blank">
                         Consulter la présentation sur le site Internet de la DREAL</a>.
-                </li><br />                
+                </li><br />
+                <?php
+                        break;
+                    case "PRA":
+                ?>
+                <li>
+                    <a href="http://paysdelaloire.lpo.fr/actions/conservation-des-especes-et-des-espaces/50-plan-d-actions-pour-les-especes" target="_blank">
+                        Consulter la présentation sur le site Internet de la <abbr  title="Ligue pour la Protection des Oiseaux">LPO</abbr> Pays de la Loire</a>.
+                </li><br />
                 <?php
                         break;
                 endswitch;
@@ -70,9 +90,11 @@ $taxons = getTaxons($type_plan,$type_sp);
             <th>Nom scientifique</th>
             <th>Nom vernaculaire</th>
             <th>Fiche INPN</th>
-            <?php if($type_plan == "PNA"): ?>
-            <th>Plan d'action</th>
-            <?php endif; ?>
+            <th>
+            <?php if($type_plan == ("PNA" || "PRA")): ?>Plan d'actions<?php endif; ?>
+            <?php if($type_plan == "PCR"): ?>Plan de conservation<?php endif; ?>
+            <?php if($type_plan == "PCL"): ?>&Eacute;tat des lieux et/ou Plan de conservation<?php endif; ?>
+            </th>
     </thead>
     <tbody>
         <?php foreach ($taxons as $taxon): ?>
@@ -119,15 +141,29 @@ $taxons = getTaxons($type_plan,$type_sp);
             <?php  else: ?>
             <td></td>
             <?php endif; ?>
-            <?php if($type_plan == "PNA"): ?>
             <td>
+            <?php if(!empty($taxon["URL"])): ?>
                 <a href="<?=$taxon["URL"]?>" target="_blank">
-                    <img src="IMG/png/gnome-globe.png" style="border:none"  alt="T&eacute;l&eacute;charger" />
-                    <strong>Consulter le plan d'actions</strong><br /> sur le 
-                    site du Minist&egrave;re</a>.
-            </td>
+                    <img src="IMG/png/gnome-globe.png" style="border:none"  alt="T&eacute;l&eacute;charger" />               
+                    <?php if($type_plan == "PNA"): ?>
+                        <strong>Consulter le plan d'actions</strong>
+                        <br /> sur le site du Minist&egrave;re</a>.
+                    <?php endif; ?>
+                    <?php if($type_plan == "PCR"): ?>
+                        <strong>Consulter le plan de conservation</strong>
+                        <br /> sur le site du <abbr  title="Conservatoire Botanique National de Brest">CBNB</abbr></a>.
+                    <?php endif; ?>
+                    <?php if($type_plan == "PCL"): ?>
+                        <strong>Consulter le document</strong></a>.                 
+                    <?php endif; ?>
+                    <?php if($type_plan == "PRA"): ?>
+                        <strong>Consulter le plan d'actions</strong></a>.                 
+                    <?php endif; ?>
             <?php endif; ?>
+            </td>
+            
         </tr>
         <?php endforeach; ?>
     </tbody>
 </table>
+<?php endif; ?>
