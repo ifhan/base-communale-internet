50206c65c096e27c1627896b0f960d3052dcdb90
Ronan Vignard <ronan.vignard@yahoo.fr>
2015-04-02 09:27:04 +0200

    Intégration de la fonction mb_strtoupper()

--- scripts/inc/zonage.inc.php
+++ scripts/inc/zonage.inc.php
@@ -3,6 +3,9 @@
 require_once(dirname(__FILE__)."/../config/constants.inc.php");
 require_once(dirname(__FILE__)."/../config/database.inc.php");
 
+// Utility functions  
+require_once(dirname(__FILE__)."/../classes/utilities.inc.php");
+
 // Classes
 require_once (dirname(__FILE__)."/../classes/Zonage.class.php");
 
@@ -16,4 +19,4 @@ $id_regional = $_REQUEST["id_regional"];
 $zonage = new Zonage();
 $zonage->getZonageByIdRegional($id_type, $id_regional);
 ?>
-<?=$zonage->id_regional?> <?=$zonage->nom?>
+<?=$zonage->id_regional?> <?= mb_strtoupper($zonage->nom) ?>
