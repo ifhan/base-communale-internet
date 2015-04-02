37a6236ba045fa9e327ffffe05f43572fc533bb7
Ronan Vignard <ronan.vignard@yahoo.fr>
2015-04-02 14:16:15 +0200

    Création du formulaire

---
+++ formulaires/type_risques_miniers.php
@@ -0,0 +1,19 @@
+<?php
+
+function formulaires_type_risques_miniers_traiter_dist() {
+    /**
+     * Permet d'utiliser le bouton "Précédent" pour revenir au formulaire
+     */
+    refuser_traiter_formulaire_ajax();
+    /**
+     * @var $id_type int Identifiant du type de zonage
+     * @var $id_dpt string Identifiant du département
+     */
+    $id_type = _request('id_type');
+    $id_dpt = _request('id_dpt');
+    return array(
+        'redirect' => 'spip.php?page=liste_zonages&id_type=' . $id_type . '&id_dpt=' . $id_dpt
+    );
+}
+
+?>
