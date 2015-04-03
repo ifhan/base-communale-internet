6035fa63af4ffd2586e9b7a6d1a725fe0e44ac2b
Ronan Vignard <ronan.vignard@yahoo.fr>
2015-04-03 16:51:39 +0200

    Commentaire

--- scripts/inc/reseau.inc.php
+++ scripts/inc/reseau.inc.php
@@ -9,6 +9,8 @@ require_once (dirname(__FILE__) . "/../classes/StationQualite.class.php");
 /**
  * Ce fichier sert à récupérer le type de réseau auquel appartient la station
  * Qualité
+ * @todo utiliser plutôt les tables r_reseau_qualite_r52 ou 
+ * r_qualite_reseaux_r52 dans le cadre d'une refonte du module
  * @var $id_regional Identifiant de la station
  */
 $id_regional = $_REQUEST["id_regional"];
