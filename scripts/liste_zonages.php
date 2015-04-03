8ca1a8b1893395e884792a3e162b0a2125bff54d
Ronan Vignard <ronan.vignard@yahoo.fr>
2015-04-03 17:22:24 +0200

    Format + Suppression de l'appel à une classe

--- scripts/liste_zonages.php
+++ scripts/liste_zonages.php
@@ -8,7 +8,6 @@ require_once 'classes/utilities.inc.php';
 
 // Classes
 require_once 'classes/Sic.class.php';
-require_once 'classes/ZnieffIp.class.php';
 require_once 'classes/Zonage.class.php';
 
 /**
@@ -18,165 +17,116 @@ require_once 'classes/Zonage.class.php';
  * @var $id_type Identifiant du type de zonage 
  */
 $id_eur15 = $_REQUEST["id_eur15"];
-$id_corine = $_REQUEST["id_corine"];
 $id_dpt = $_REQUEST["id_dpt"];
 $id_type = $_REQUEST["id_type"];
 ?>
-<?php
-if (isset($id_eur15)):
-    /**
-     * I. Affiche les sites Natura 2000 par habitat de la nomenclature EUR15 
-     * Seule la Directive Habitats est concernee : lien vers les SIC 
-     * @todo prévoir un lien vers les ZSC 
-     */
-    $array_sic = getSicByIdEur15($id_eur15);
-    ?>
-    <?php if (count($array_sic) > 0): ?>
-        <br />
-        <table class="display" id="example">
-            <thead>
-                <tr>
-                    <th>Identifiant</th>
-                    <th>Nom</th>
-                    <th>Fiche</th>
-                </tr>
-            </thead>
-            <tbody>
-                    <?php foreach ($array_sic as $sic): ?>
-                    <tr valign="top">
-                        <?php if (isset($sic["id_regional"])): ?>
-                            <td><?= $sic["id_regional"]; ?></td>
-            <?php endif; ?>
-                        <td width="100%"><?= $sic["nom"]; ?></td>
-                        <td class="cache">
-                            <div align="right">
-                                <a href="spip.php?page=zonage&id_type=<?= $sic["id_type"]; ?>&amp;id_regional=<?= $sic["id_regional"]; ?>"><img src="IMG/png/system-search.png" alt="Icone Afficher" /></a>
-                                <br />
-                            </div>
-                        </td>
-                    </tr>
-        <?php endforeach; ?>
-        </table>
-    <?php else: ?>
-        <p>Cet habitat n'a pas &eacute;t&eacute; recens&eacute; en Pays de la Loire.</p>
-    <?php endif; ?>
-<?php
-elseif (isset($id_corine)):
-    /**
-     * II. Affiche les ZNIEFF par habitat de la nomenclature CORINE 
-     */
-    $array_znieff = getZnieffByIdCorine($id_corine);
-    ?>    
-        <?php if (count($array_znieff) > 0): ?>
-        <br />
-        <table class="encadre">
-                <?php foreach ($array_znieff as $znieff): ?>
-                <tr bgcolor="<?= switchColor() ?>" valign=""top>
-            <?php if ($znieff["NM_REGZN"] != "0"): ?>
-                        <td>
-                            <strong><?= $znieff["NM_REGZN"] ?></strong>
-                        </td>
-            <?php endif; ?>
-                    <td width="99%">&nbsp;<?= $znieff["LB_ZN"]; ?></td>
-                    <td class="cache">
-                        <div align="right">
-                            <?php
-                            // Sélection du type de zonage pour le lien hypertexte
-                            if ($znieff["TY_ZONE"] == "1"):
-                                $id_type = "10";
-                            elseif ($znieff["TY_ZONE"] == "2"):
-                                $id_type = "11";
-                            endif;
-                            ?>
-                            <a href="spip.php?page=zonage&id_type=<?= $id_type; ?>&amp;id_regional=<?= $znieff["NM_REGZN"] ?>"><img src="IMG/png/system-search.png" alt="Icone Afficher" /></a><br />
-                        </div>
-                    </td>
-                </tr>
-        <?php endforeach; ?>
-        </table>
-    <?php else: ?>
-        <p>Cet habitat n'a pas &eacute;t&eacute; recens&eacute; en Pays de la Loire.</p>
-    <?php endif; ?>
-<?php
-elseif ($id_dpt != "0"):
-
-    /**
-     * III. Affichage du type de zonage sélectionné dans le formulaire 
-     * pour le département requis ou la région	
-     */
-    ?>
-    <?php $zonages = getZonagesByIdTypeByIdDpt($id_type, $id_dpt); ?>
-    <?php // Affiche le nombre de résultats retourne par la requête  ?>
-    <p>
-        <strong><?= count($zonages) ?> enregistrement(s)</strong> 
-        recens&eacute;(s) sur l'aire g&eacute;ographique choisie.
-    </p>
-    <?php if (count($zonages) > 0): ?>
-        <br />
-        <table class="display" id="example">
-            <thead>
-                <tr>
-                    <th>Identifiant</th>
-                    <th>Nom</th>
-                    <th>Fiche</th>
-                </tr>
-            </thead>
-            <tbody>
-                    <?php foreach ($zonages as $zonage): ?>
-                    <tr valign="top">
-                        <?php if ($zonage["id_regional"] != "0") : ?>
-                            <td><?= $zonage["id_regional"] ?> </td>
-            <?php endif; ?>
-                        <td width="100%">&nbsp;<?= mb_strtoupper($zonage["nom"]) ?></td>
-                        <td class="cache">
-                            <div align="right">
-                                <a href="spip.php?page=zonage&id_type=<?= $id_type ?>&amp;id_regional=<?= $zonage["id_regional"] ?>"><img src="IMG/png/system-search.png"  alt="Icone afficher la fiche du zonage" /></a><br />
-                            </div>
-                        </td>
-                    </tr>
-        <?php endforeach; ?>		
-            </tbody>
-        </table>
-    <?php else: echo ""; ?>
-    <?php endif; ?>
-<?php
-else:
-    /**
-     * III. B. Sélection de la région
-     */
-    ?>
-    <?php $zonages = getZonagesByIdTypeByRegion($id_type); ?>
-    <?php // Affiche le nombre de résultats retourne par la requête  ?>
-    <p>
-        <strong><?= count($zonages) ?> enregistrement(s)</strong> 
-        recens&eacute;(s) sur l'aire g&eacute;ographique choisie.
-    </p>
-    <?php if (count($zonages) > 0): ?>
-        <br />
-        <table class="display" id="example">
-            <thead>
-                <tr>
-                    <th>Identifiant</th>
-                    <th>Nom</th>
-                    <th>Fiche</th>
-                </tr>
-            </thead>
-            <tbody>
-                    <?php foreach ($zonages as $zonage): ?>
-                    <tr valign="top">
-            <?php if ($zonage["id_regional"] != "0"): ?>
-                            <td><?= $zonage["id_regional"] ?></td>
-            <?php endif; ?>
-                        <td width="99%">&nbsp;<?= mb_strtoupper($zonage["nom"]) ?></td>
-                        <td class="cache">
-                            <div align="right">
-                                <a href="spip.php?page=zonage&id_type=<?= $id_type ?>&amp;id_regional=<?= $zonage["id_regional"] ?>"><img src="IMG/png/system-search.png" alt="Icone" /></a><br />
-                            </div>
-                        </td>
-                    </tr>
-        <?php endforeach; ?>
-            </tbody>
-        </table>
-    <?php else: echo ""; ?>
-    <?php endif; ?>
+<!-- 1. Affiche les sites Natura 2000 par habitat de la nomenclature EUR15 
+     Seule la Directive Habitats est concernée : lien vers les SIC 
+     @todo prévoir un lien vers les ZSC -->
+<?php if(isset($id_eur15)): ?>
+<?php $array_sic = getSicByIdEur15($id_eur15); ?>
+<?php if (count($array_sic) > 0): ?><br />
+<table class="display" id="example">
+    <thead>
+        <tr>
+            <th>Identifiant</th>
+            <th>Nom</th>
+            <th>Fiche</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($array_sic as $sic): ?>
+        <tr valign="top">
+        <?php if (isset($sic["id_regional"])): ?>
+            <td><?= $sic["id_regional"]; ?></td>
+        <?php endif; ?>
+            <td width="100%"><?= $sic["nom"]; ?></td>
+            <td class="cache">
+                <div align="right">
+                    <a href="spip.php?page=zonage&id_type=<?= $sic["id_type"]; ?>&amp;id_regional=<?= $sic["id_regional"]; ?>">
+                        <img src="IMG/png/system-search.png" alt="Icone Afficher" />
+                    </a><br />
+                </div>
+            </td>
+        </tr>
+    <?php endforeach; ?>
+</table>
+<?php else: ?>
+<p>Cet habitat n'a pas &eacute;t&eacute; recens&eacute; en Pays de la Loire.</p>
+<?php endif; ?>
+<!-- 2. Affichage du type de zonage sélectionné dans le formulaire -->
+<!-- 2.1 Affichage pour un département -->
+<?php elseif($id_dpt != "0"): ?>
+<?php $zonages = getZonagesByIdTypeByIdDpt($id_type, $id_dpt); ?>
+<!-- Affiche le nombre de résultats retourne par la requête  -->
+<p>
+    <strong><?= count($zonages) ?> enregistrement(s)</strong> 
+    recens&eacute;(s) sur l'aire g&eacute;ographique choisie.
+</p>
+<?php if(count($zonages)>0): ?><br />
+<table class="display" id="example">
+    <thead>
+        <tr>
+            <th>Identifiant</th>
+            <th>Nom</th>
+            <th>Fiche</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($zonages as $zonage): ?>
+        <tr valign="top">
+        <?php if ($zonage["id_regional"] != "0") : ?>
+            <td><?= $zonage["id_regional"] ?> </td>
+        <?php endif; ?>
+            <td width="100%">&nbsp;<?= mb_strtoupper($zonage["nom"]) ?></td>
+            <td class="cache">
+                <div align="right">
+                    <a href="spip.php?page=zonage&id_type=<?= $id_type ?>&amp;id_regional=<?= $zonage["id_regional"] ?>">
+                        <img src="IMG/png/system-search.png"  alt="Icone afficher la fiche du zonage" />
+                    </a><br />
+                </div>
+            </td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+<?php else: echo ""; ?>
+<?php endif; ?>
+<!-- 2.2 Sélection de la région -->
+<?php else: ?>
+<?php $zonages = getZonagesByIdTypeByRegion($id_type); ?>
+<!-- Affiche le nombre de résultats retourne par la requête  -->
+<p>
+    <strong><?= count($zonages) ?> enregistrement(s)</strong> 
+    recens&eacute;(s) sur l'aire g&eacute;ographique choisie.
+</p>
+<?php if(count($zonages)>0): ?><br />
+<table class="display" id="example">
+    <thead>
+        <tr>
+            <th>Identifiant</th>
+            <th>Nom</th>
+            <th>Fiche</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($zonages as $zonage): ?>
+        <tr valign="top">
+        <?php if ($zonage["id_regional"] != "0"): ?>
+            <td><?= $zonage["id_regional"] ?></td>
+        <?php endif; ?>
+            <td width="99%">&nbsp;<?= mb_strtoupper($zonage["nom"]) ?></td>
+            <td class="cache">
+                <div align="right">
+                    <a href="spip.php?page=zonage&id_type=<?= $id_type ?>&amp;id_regional=<?= $zonage["id_regional"] ?>">
+                        <img src="IMG/png/system-search.png" alt="Icone" />
+                    </a><br />
+                </div>
+            </td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+<?php else: echo ""; ?>
+<?php endif; ?>
 <?php endif; ?>
