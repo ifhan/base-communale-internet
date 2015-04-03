d0e471f03d7f792159ee062cbc4c6e48e51c28e7
Ronan Vignard <ronan.vignard@yahoo.fr>
2015-04-03 17:22:09 +0200

    Suppression de l'appel à une classe

--- scripts/inc/type_zonage.inc.php
+++ scripts/inc/type_zonage.inc.php
@@ -6,11 +6,9 @@ require_once(dirname(__FILE__)."/../config/database.inc.php");
 // Classes
 require_once(dirname(__FILE__)."/../classes/Zonage.class.php");
 require_once(dirname(__FILE__)."/../classes/HabitatEur15.class.php");
-require_once(dirname(__FILE__)."/../classes/HabitatCorine.class.php");
 
 $id_type = $_REQUEST["id_type"];
 $id_eur15 = $_REQUEST["id_eur15"];
-$id_corine = $_REQUEST["id_corine"];
 $type_plan = $_REQUEST["type_plan"];
 
 if(isset($id_type)) {
@@ -23,12 +21,6 @@ if(isset($id_eur15)) {
     $habitat_eur15 = new HabitatEur15();
     $habitat_eur15->getHabitatEur15ById($id_eur15);
     echo $habitat_eur15->lb_eur15;
-}
-	
-if(isset($id_corine)) {
-    $habitat_corine = new HabitatCorine();
-    $habitat_corine->getHabitatCorineById($id_corine);
-    echo $habitat_corine->id_corine." - ".$habitat_corine->lb_corine;
 }
 
 if(isset($type_plan)) {
