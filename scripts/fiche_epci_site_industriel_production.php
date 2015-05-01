e02df43e0a7ffbcb4c9152367688e0f187ad7229
Ronan Vignard <ronan.vignard@yahoo.fr>
2015-05-01 15:47:29 +0200

    Ajout d'un accès direct

--- scripts/fiche_epci_site_industriel_production.php
+++ scripts/fiche_epci_site_industriel_production.php
@@ -15,42 +15,43 @@ require_once 'classes/Zonage.class.php';
  */
 $id_epci = $_REQUEST["id_epci"];
 
-$themes = getThemesByIdEpciIdRubrique($id_epci,15);
+$themes = getThemesByIdEpciByIdRubrique($id_epci,15);
 ?>
 <?php if(count($themes) > 0): ?>
+    <!-- Accès direct par thème en haut de page -->
+    <div class="listerub">
+        <div class="titresousrub">Accès direct</div>
+        <div id="plan2">
+            <ul class="plansite">
+                <?php foreach($themes as $theme): ?>
+                <li>
+                    <a href="#<?=$theme["id_theme"]?>" class="plansecteur"><?=$theme["theme"]?></a>
+                        <?php $id_theme = $theme["id_theme"]; ?>
+                        <?php $types_zonages = getTypesZonagesByIdEpciByIdTheme($id_epci, $id_theme) ?>
+                    <ul>
+                        <?php foreach($types_zonages as $type_zonage): ?>
+                        <li><a href="#<?=$type_zonage["id_type"]?>" class="planrubniv1"><?=$type_zonage["type"]?></a></li>
+                        <?php endforeach; ?>
+                    </ul>
+                </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    </div><br />
     <?php foreach($themes as $theme): ?>
     <div class="listerub">
         <div class="titresousrub"><?=$theme["theme"]?></div>
-        <?php
-        /**
-         * Affichage du thème des types de zonage 
-         */
-        ?>
-        <?php $id_theme = $theme["id_theme"]; ?>
+        <!-- Affichage du thème des types de zonage -->
+        <div id="<?=$theme["id_theme"]?>"><?php $id_theme = $theme["id_theme"]; ?>
         <?php $types_zonages = getTypesZonagesByIdEpciByIdTheme($id_epci, $id_theme) ?>
         <?php foreach($types_zonages as $type_zonage): ?>
-            <?php
-            /**
-             * Affichage du type de zonage 
-             */
-            ?>
-            <strong><?=$type_zonage["type"]?> :</strong><br />
+            <!-- Affichage du type de zonage -->
+            <div id="<?=$type_zonage["id_type"]?>"></div><strong><?=$type_zonage["type"]?> :</strong><br />
             <?php $id_type = $type_zonage["id_type"]; ?>
             <?php $zonages = getZonagesByIdTypeByIdEpci($id_type, $id_epci) ?>
             <table class="encadre">
             <?php foreach($zonages as $zonage): ?>
-                <?php
-                /**
-                * Affichage d'un commentaire spécifique pour le PNA Luronium natans
-                */
-                ?>
-                
-                <?php
-                /**
-                 * Affichage de l'identifiant, du nom du zonage et 
-                 * du lien vers la liste des ressources associées
-                 */
-                ?>
+                <!-- Affichage de l'identifiant, du nom du zonage et du lien vers la liste des ressources associées -->
                 <tr bgcolor="<?=switchColor()?>" valign="top">
                     <td>
                         <strong><?= $zonage["id_regional"] ?></strong>
