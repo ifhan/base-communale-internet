d60fedafc9121ad899b09ccbbdb83ff549673710
Ronan Vignard <ronan.vignard@yahoo.fr>
2015-05-12 14:39:18 +0200

    First commit

---
+++ scripts/fiche_unesco.php
@@ -0,0 +1,41 @@
+<?php
+// Application-wide data and database connection
+require_once 'config/constants.inc.php';
+require_once 'config/database.inc.php';
+
+// Classes
+require_once 'classes/Departement.class.php';
+require_once 'classes/Unesco.class.php';
+
+/**
+ * @var $id_type Identifiant du type de zonage
+ * @var $id_regional Identifiant régional du zonage
+ */
+$id_type = $_REQUEST["id_type"];
+$id_regional = $_REQUEST["id_regional"];
+$unesco = new Unesco();
+$unesco->getUnescoByIdRegional($id_regional);
+
+$departement = new Departement();
+$departement->getDepartementByIdRegional($id_regional,$id_type);
+?>
+<table class="cadre_plein">
+    <tr>
+        <td>Nom&nbsp;:</td>
+        <td><strong><?=$unesco->nom?></strong></td>
+    </tr>
+    <tr>
+        <td>Identifiant r&eacute;gional&nbsp;:</td>
+        <td><strong><?=$unesco->id_regional?></strong></td>
+    </tr>
+    <tr>
+        <td>D&eacute;partement&nbsp;: </td>
+        <td>
+            <strong>
+            <?=$departement->nom_departement?> (<?=$departement->id_departement?>)
+            </strong>
+        </td>
+    </tr>
+</table>
+<h3 class="spip">Commune(s) concern&eacute;e(s)&nbsp;:</h3>
+<?php require_once 'inc/commune.inc.php'; ?>
