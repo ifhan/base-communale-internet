a756d3e5af15c864897d3d08c8cc4550a75ec92a
Ronan Vignard <ronan.vignard@yahoo.fr>
2015-05-19 14:42:05 +0200

    Modification de la classe

--- scripts/classes/Sage.class.php
+++ scripts/classes/Sage.class.php
@@ -5,8 +5,8 @@
  * Classe et fonctions concernant les Schémas d'Aménagement et de Gestion
  * des Eaux (SAGE)
  * @author Ronan Vignard <ronan.vignard@developpement-durable.gouv.fr>
- * @copyright 2015-04-02
- * @version 1.1
+ * @copyright 2015-05-19
+ * @version 1.2
  */
 class Sage {
 
@@ -17,64 +17,18 @@ class Sage {
     public function getSageByIdRegional($id_regional) {
         $pdo = ConnectionFactory::getFactory()->getConnection();
         $sql = $pdo->prepare('SELECT * 
-        FROM r_sage_r52, r_sage_r52_data  
-        WHERE r_sage_r52.id_regional = :id_regional
-        AND r_sage_r52.id_regional = r_sage_r52_data.id_regional');
+        FROM n_sage_r52
+        WHERE n_sage_r52.id_regional = :id_regional');
         $sql->bindParam(':id_regional', $id_regional, PDO::PARAM_STR, 10);
         try {
             $sql->execute();
             $row = $sql->fetch();
-            $this->avancement = $row['avancement'];
+            $this->id_regional = $row['id_regional'];
             $this->nom = $row['nom'];
-            $this->referent_sema = $row['referent_sema'];
-            $this->maitre_ouvrage = nl2br($row['maitre_ouvrage']);
-            $this->president = nl2br($row['president']);
-            $this->animateur = nl2br($row['animateur']);
-            $this->adresse = nl2br($row['adresse']);
-            $this->tel = nl2br($row['tel']);
-            $this->fax = nl2br($row['fax']);
-            $this->courriel = nl2br($row['courriel']);
-            $this->enjeux = nl2br($row['sdage']);
-            $this->bassin_versant = nl2br($row['bassin_versant']);
-            $this->rivieres = nl2br($row['rivieres']);
-            $this->nombre_departements = nl2br($row['nombre_departements']);
-            $this->nombre_communes = nl2br($row['nombre_communes']);
-            $this->population = nl2br($row['population']);
-            $this->demandeur = nl2br($row['demandeur']);
-            $this->comite_bassin = nl2br($row['comite_bassin']);
-            $this->prefecture_pilote = nl2br($row['prefecture_pilote']);
-            $this->arrete_perimetre = nl2br($row['arrete_perimetre']);
-            $this->arrete_cle = nl2br($row['arrete_cle']);
-            $this->installation_cle = nl2br($row['installation_cle']);
-            $this->diagnostic = nl2br($row['diagnostic']);
-            $this->enjeux_retenus = nl2br($row['enjeux_retenus']);
-            $this->strategie = nl2br($row['strategie']);
-            $this->validation = nl2br($row['validation']);
-            $this->consultation = nl2br($row['consultation']);
-            $this->date_comite_bassin = nl2br($row['date_comite_bassin']);
-            $this->arrete_sage = nl2br($row['arrete_sage']);
-            $this->url_site_web = nl2br($row['url_site_web']);
-            $this->documents = nl2br($row['documents']);
-            $this->priorites = nl2br($row['priorites']);
-        } catch (PDOException $e) {
-            echo 'ERROR: ' . $e->getMessage();
-        }
-    }
-
-    /**
-     * Sélectionne la situation d'un SAGE par son identifiant
-     * @param int $id_situation Identifiant de la situation du SAGE
-     */
-    public function getSageSituationByIdSituation($id_situation) {
-        $pdo = ConnectionFactory::getFactory()->getConnection();
-        $sql = $pdo->prepare('SELECT * FROM r_sage_situation_r52 
-        WHERE id_situation = :id_situation');
-        $sql->bindParam(':id_situation', $id_situation, PDO::PARAM_INT, 11);
-        try {
-            $sql->execute();
-            $row = $sql->fetch();
-            $this->nom_situation = $row['nom_situation'];
-            $this->code_couleur = $row['code_couleur'];
+            $this->etat = $row['etat'];
+            $this->comite = $row['comite'];
+            $this->type_perimetre = $row['type_perimetre'];
+            $this->url_gesteau = $row['url_gesteau'];
         } catch (PDOException $e) {
             echo 'ERROR: ' . $e->getMessage();
         }
