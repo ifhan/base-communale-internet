704dd29b3f08043c833099ac75ff2bc989dfbf03
Ronan Vignard <ronan.vignard@yahoo.fr>
2017-03-13 11:48:10 +0100

    Modification du nom de la table

--- scripts/classes/Zsc.class.php
+++ scripts/classes/Zsc.class.php
@@ -5,8 +5,8 @@
  * Classe et fonctions concernant les Zones Spéciales de Conservation (ZSC)
  * (Natura 2000, directive "Habitats")
  * @author Ronan Vignard <ronan.vignard@developpement-durable.gouv.fr>
- * @copyright 2012-06-26
- * @version 1.0
+ * @copyright 2016-12-23
+ * @version 1.1
  */
 class Zsc {
 
@@ -16,7 +16,7 @@ class Zsc {
      */
     public function getZscDataByIdRegional($id_regional) {
         $pdo = ConnectionFactory::getFactory()->getConnection();
-        $sql = $pdo->prepare('SELECT * FROM R_ZSC_R52_data 
+        $sql = $pdo->prepare('SELECT * FROM r_zsc_r52_data 
         WHERE id_regional = :id_regional');
         $sql->bindParam(':id_regional', $id_regional, PDO::PARAM_STR, 10);
         try {
@@ -24,9 +24,9 @@ class Zsc {
             $row = $sql->fetch();
             $this->id_regional = $row["id_regional"];
             $this->nom = $row["nom"];
-            $this->dreal = $row["dreal"];
-            $this->url_fiche = $row["url_fiche"];
-            $this->url_docob = $row["url_docob"];
+            $this->id_article = $row["id_article"];
+            $this->id_side = $row["id_side"];
+            $this->id_type = $row["id_type"];
         } catch (PDOException $e) {
             echo 'ERROR: ' . $e->getMessage();
         }
